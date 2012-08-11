0950bdfec2437a316a357845e5ea2d4ee0f9ba17
Aram Zucker-Scharff <azuckers@gmu.edu>
2012-08-11 19:30:12 -0400

    Begin the plugin

---
+++ rssforward.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+Plugin Name: RSS to PressForward
+Plugin URI: http://aramzs.me
+Description: This plugin is a tutorial for THATCamp. 
+Version: 0.2
+Author: Aram Zucker-Scharff
+Author URI: http://aramzs.me
+License: GPL2
+*/
+
+/*  Developed for the Center for History and New Media
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+//Set some variables for likely later re-use. 
+
+//First create the plugin menu
+add_menu_page($rsspf_title
+
+?>
