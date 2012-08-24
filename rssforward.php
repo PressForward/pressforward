51242b89f2ec64506e86fbc58db8f8d18fdb2a6e
Aram Zucker-Scharff <azuckers@gmu.edu>
2012-08-24 18:28:40 -0400

    handling postthumb passage - rss to post types

--- rssforward.php
+++ rssforward.php
@@ -422,6 +422,45 @@ class rsspf {
 	
 	}
 	
+	function set_me_as_featured($postID,$img){
+		
+		//Methods within sourced from http://codex.wordpress.org/Function_Reference/wp_insert_attachment
+		//and http://wordpress.stackexchange.com/questions/26138/set-post-thumbnail-with-php
+		
+		//Get the type of the image file. .jpg, .gif, or whatever
+		$filetype = wp_check_filetype( $img );
+		$uploadDir = wp_upload_dir();
+		//Set the identifying variables for the about to be featured image.
+		$imgData = array(
+						//tell WordPress what the filetype is. 
+						'post_mime_type' => $filetype['type'],
+						//set the image title to the title of the site you are pulling from
+						'post_title' => $name,
+						//WordPress tells us we must set this and set it to empty. Why? Dunno.
+						'post_content' => '',
+						//Now we set the status of the image. It will inheret that of the post.
+						//If the post is published, then the image will be to.
+						'post_status' => 'inherit'
+					);
+		//WordPress needs an absolute path to the image, as opposed to the relative path we used before. 
+		//I'm hoping that by using the upload_dir function (above) I can make this function work with multisite. 
+		$pathedImg = $uploadDir['url'] . $img;
+		//Now we insert the image as a WordPress attachement, and associate it with the current post. 
+		$thumbid = wp_insert_attachment($imgData, $pathedImg, $postID);
+		
+		//To set a thumbnail, you need metadata associated with an image. 
+		//To get that we need to call the image.php file
+		require_once(ABSPATH . 'wp-admin/includes/image.php');
+		$metadata = wp_generate_attachment_metadata( $thumbid, $pathedImg );
+		//Now we attach the meta data to the image.
+		wp_update_attachment_metadata( $thumbid, $metadata );
+		
+		//Now that we have a correctly meta-ed and attached image we can finally turn it into a post thumbnail.
+		update_post_meta($postID, '_thumbnail_id', $thumbid);
+
+
+	}	
+	
 	function build_a_nomination() {
 		
 		// Verify nonce
@@ -501,6 +540,8 @@ class rsspf {
 		);
 		
 		$newNomID = wp_insert_post( $data );
+		
+		$this->set_me_as_featured($newNomID, $_POST['item_feat_img']);
 
 		add_post_meta($newNomID, 'origin_item_ID', $item_id, true);
 		add_post_meta($newNomID, 'nomination_count', 1, true);
@@ -517,7 +558,7 @@ class rsspf {
 	}
 	
 	function send_nomination_for_publishing() {
-	
+		global $post;
 		// verify if this is an auto save routine. 
 		// If it is our form has not been submitted, so we dont want to do anything
 		//if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) 
@@ -553,6 +594,13 @@ class rsspf {
 				add_post_meta($newPostID, 'submitted_by', $userID, true);
 				$item_permalink = get_post_meta($_POST['ID'], 'nomination_permalink', true);
 				add_post_meta($newPostID, 'nomination_permalink', $item_permalink, true);
+
+				$already_has_thumb = has_post_thumbnail($_POST['ID']);
+				if ($already_has_thumb)  {
+					$post_thumbnail_id = get_post_thumbnail_id( $_POST['ID'] );
+					set_post_thumbnail($newPostID, $post_thumbnail_id);
+				}
+				
 			}
 		}
 	
