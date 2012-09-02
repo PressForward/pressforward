024f70a724d244b1584326b14d95e3b13da578a1
Aram Zucker-Scharff <azuckers@gmu.edu>
2012-09-02 03:15:51 -0400

    Removed unused jquery response.

--- includes/js/nomination-imp.js
+++ includes/js/nomination-imp.js
@@ -41,10 +41,6 @@ jQuery(document).ready(function() {
 		evt.preventDefault();
 		jQuery.post(ajaxurl, {
 			action: 'assemble_feed_for_pull'
-		},
-		function(response) {
-			alert(response);
-			//jQuery("#test-div1").append(data);
 		});
 	
 	});
