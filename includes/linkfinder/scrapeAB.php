150a69b1611842e0797170979cf86f6c27aa758f
Aram Zucker-Scharff <azuckers@gmu.edu>
2012-09-20 14:34:56 -0400

    Testing building scrapper array.

--- includes/linkfinder/scrapeAB.php
+++ includes/linkfinder/scrapeAB.php
@@ -29,21 +29,112 @@ function customError($errno, $errstr)
 //Random article for testing.
 $html = file_get_html('http://academicblogs.org/index.php/Main_Page');
 echo 'Blog Categories: <br />'; 
+# Get the title page
 foreach ($html->find('h1') as $link){
 	
 //	if (($link->plaintext == '[edit] External links') || ($link->plaintext == '[edit] References') ){
 		echo '<br /><br />Innertext: ';
 		print_r($link->innertext);
 		echo '<br /><br />';
-
+		
+		# Get the main content block
 		$nextBlock = $link->next_sibling();
 		//print_r($nextBlock);	
 		
+		$htmlCounter = array();
+		$counter = 0;
+		$sectionCounter = 0;
+		$links = array();
+		# Walk through the dom and count paragraphs between H2 tags
+		foreach ($nextBlock->children() as $bodyChild) {
+			echo $bodyChild;
+			
+			if (($bodyChild->find('span')) && ($bodyChild->tag=='h2')){
+				foreach ($bodyChild->find('span') as $span){
+					$sectionCounter++;
+					$spanText = $span->innertext;
+					
+					$spanNameArray = explode(' ', $spanText);
+					$spanSlug = implode('', $spanNameArray);
+					
+					echo $spanSlug;
+					echo ' < The Span Slug.<br />';
+					
+					$htmlCounter[$spanSlug]['slug'] = $spanSlug;
+					$htmlCounter[$spanSlug]['text'] = $spanText;
+					$htmlCounter[$spanSlug]['counter'] = $counter;
+					$counter = 0;
+					$links = array();
+					//$htmlCounter[];
+				}
+			}
+			
+			
+			if (($bodyChild->tag=='p') && ((count($bodyChild->find('a'))) == 1)){
+				echo count($bodyChild->find('a'));
+				$counter++;
+				echo '<br />' . $counter . '<br />';
+				foreach ($bodyChild->find('a') as $childLink){
+					echo $link = $childLink->href;
+					echo $title = $childLink->title;
+					
+					$titleArray = explode(' ', $title);
+					$titleSlug = implode('', $titleArray);
+					$sectionSlug = $htmlCounter[$spanSlug]['slug'];
+					
+					$htmlCounter[$spanSlug]['links'][$titleSlug]['slug'] = $titleSlug;
+					$htmlCounter[$spanSlug]['links'][$titleSlug]['title'] = $title;
+					$htmlCounter[$spanSlug]['links'][$titleSlug]['link'] = $link;
+					
+					$links[$sectionSlug][$titleSlug]['title'] = $title;
+					$links[$sectionSlug][$titleSlug]['link'] = $link;
+				}
+			}			
+			
+			echo '<hr />';
+		}
+		
+		echo '<pre>';
+		print_r($htmlCounter);
+		echo '</pre>';
+		
+		
+		# Get the links.
 		foreach ($nextBlock->find('h2') as $h2) {
 				echo 'H2 Head: ';
 				print_r($h2->innertext);
 				echo '<br /><br />';	
+
+				if ($h2->next_sibling() != ''){
+					//while(count($h2Under->find('span')) != 0){
+						$h2Under = $h2->next_sibling();
+						echo $h2Under;
+						//if (empty($h2Under->find('span'))){
+							echo '<br />' . count($h2Under->find('span')) . '< Is this a span';
+						//}
+						echo $h2Under->next_sibling();
+						
+						foreach ($h2Under->find('a') as $anInnerLink){
+							if($anInnerLink->getAttribute('rel') != 'nofollow'){
+								if (($anInnerLink->href != '') && ($anInnerLink->title != '')) {
+									$theExternalSiteLink = $anInnerLink->href;
+									echo $h2->innertext;
+									echo $anInnerLink->title;
+									echo ' - ';
+									echo $theExternalSiteLink;
+									echo '<br /><br /><br />';
+									
+								}
+							}
+						
+						}
+					//}
+				}
+			
 		}
+		
+		echo '<br /> <strong>Other tests:</strong> <br /><br />';
+		
 		foreach ($nextBlock->find('a') as $innerLink){
 		if($innerLink->getAttribute('rel') != 'nofollow'){
 			if (($innerLink->href != '') && ($innerLink->title != '')) {
