49e14d0aadb10eb6a5b8402f9a7fe2d0d4693c76
Aram Zucker-Scharff <azuckers@gmu.edu>
2012-09-20 17:34:54 -0400

    Working scraper for  Academic blogs.

--- includes/linkfinder/scrapeAB.php
+++ includes/linkfinder/scrapeAB.php
@@ -24,6 +24,69 @@ function customError($errno, $errstr)
   return 'Nothing found';
 
 }
+# via http://stackoverflow.com/questions/2668854/sanitizing-strings-to-make-them-url-and-filename-safe
+function sanitize($string, $force_lowercase = true, $anal = false) {
+    $strip = array("~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "=", "+", "[", "{", "]",
+                   "}", "\\", "|", ";", ":", "\"", "'", "&#8216;", "&#8217;", "&#8220;", "&#8221;", "&#8211;", "&#8212;",
+                   "—", "–", ",", "<", ".", ">", "/", "?");
+    $clean = trim(str_replace($strip, "", strip_tags($string)));
+    $clean = preg_replace('/\s+/', "-", $clean);
+    $clean = ($anal) ? preg_replace("/[^a-zA-Z0-9]/", "", $clean) : $clean ;
+    return ($force_lowercase) ?
+        (function_exists('mb_strtolower')) ?
+            mb_strtolower($clean, 'UTF-8') :
+            strtolower($clean) :
+        $clean;
+}
+
+function slugger($string){
+
+	$string = strip_tags($string);
+	$stringArray = explode(' ', $string);
+	$stringSlug = '';
+	foreach ($stringArray as $stringPart){
+		$stringSlug .= ucfirst($stringPart);
+	}
+	$stringSlug = str_replace('&amp;','&', $stringSlug);
+	//$charsToElim = array('?','/','\\');
+	$stringSlug = sanitize($stringSlug, false, true);
+	
+	return $stringSlug;
+	
+}
+
+$spamsites = array('http://www.buy-wellbutrin.com/', 'http://www.mycaal.com/');
+
+# to fill the blog property of the array. 
+# PS... How often does this get updated?
+function getLinksFromSection ($sectionURL){		
+	global $spamsites;
+	set_time_limit(0);
+	$html = file_get_html($sectionURL);
+	
+	$blogs = array();
+	$c = 0;
+	foreach ($html->find('#bodyContent') as $body){
+		foreach ($body->find('a') as $link){
+			if (!in_array(($link->href), $spamsites)){ 
+				if ($link->rel == 'nofollow'){
+					$URL = $link->href;
+					$title = $link->innertext;
+					$slug = slugger($title);
+					$blogs[$slug]['slug'] = $slug;
+					$blogs[$slug]['url'] = $URL;
+					$blogs[$slug]['title'] = htmlspecialchars(strip_tags($title));
+				}
+			}
+			else {
+				
+			}
+		}
+	}
+	
+	return $blogs;
+	
+}
 
 
 //Random article for testing.
@@ -55,13 +118,16 @@ foreach ($html->find('h1') as $link){
 					$spanText = $span->innertext;
 					
 					$spanNameArray = explode(' ', $spanText);
-					$spanSlug = implode('', $spanNameArray);
-					
+					$spanSlug = '';
+					foreach ($spanNameArray as $spanNamePart){
+						$spanSlug .= htmlentities(ucfirst($spanNamePart));
+					}
+					$spanSlug = sanitize($spanSlug, false, true);
 					echo $spanSlug;
 					echo ' < The Span Slug.<br />';
 					
 					$htmlCounter[$spanSlug]['slug'] = $spanSlug;
-					$htmlCounter[$spanSlug]['text'] = $spanText;
+					$htmlCounter[$spanSlug]['text'] = htmlspecialchars(strip_tags($spanText));
 					$htmlCounter[$spanSlug]['counter'] = $counter;
 					$counter = 0;
 					$links = array();
@@ -69,8 +135,7 @@ foreach ($html->find('h1') as $link){
 				}
 			}
 			
-			
-			if (($bodyChild->tag=='p') && ((count($bodyChild->find('a'))) == 1)){
+			if (($bodyChild->tag=='p') && ((count($bodyChild->find('a'))) == 1) && ((count($bodyChild->find('a[class=new]'))) == 0)){
 				echo count($bodyChild->find('a'));
 				$counter++;
 				echo '<br />' . $counter . '<br />';
@@ -78,21 +143,39 @@ foreach ($html->find('h1') as $link){
 					echo $link = $childLink->href;
 					echo $title = $childLink->title;
 					
-					$titleArray = explode(' ', $title);
-					$titleSlug = implode('', $titleArray);
-					$sectionSlug = $htmlCounter[$spanSlug]['slug'];
-					
-					$htmlCounter[$spanSlug]['links'][$titleSlug]['slug'] = $titleSlug;
-					$htmlCounter[$spanSlug]['links'][$titleSlug]['title'] = $title;
-					$htmlCounter[$spanSlug]['links'][$titleSlug]['link'] = $link;
-					
-					$links[$sectionSlug][$titleSlug]['title'] = $title;
-					$links[$sectionSlug][$titleSlug]['link'] = $link;
+					if (!in_array($link, $spamsites)){
+						$titleArray = explode(' ', $title);
+						$titleSlug = '';
+						foreach ($titleArray as $titlePart){
+							$titleSlug .= htmlentities(ucfirst($titlePart));
+						}
+						//$charsToElim = array('?','/','\\');
+						$titleSlug = sanitize($titleSlug, false, true);
+						
+						$link = 'http://academicblogs.org' . $link;
+						
+						$sectionSlug = $htmlCounter[$spanSlug]['slug'];
+						
+						$htmlCounter[$spanSlug]['links'][$titleSlug]['slug'] = $titleSlug;
+						$htmlCounter[$spanSlug]['links'][$titleSlug]['title'] = htmlspecialchars(strip_tags($title));
+						$htmlCounter[$spanSlug]['links'][$titleSlug]['link'] = $link;
+						//if ($childLink->){
+							$htmlCounter[$spanSlug]['links'][$titleSlug]['blogs'] = getLinksFromSection($link);
+						//}
+						
+						$links[$sectionSlug][$titleSlug]['title'] = $title;
+						$links[$sectionSlug][$titleSlug]['link'] = $link;
+					} else {
+						echo 'Spam Site';
+						$counter--;
+					}
 				}
 			}			
 			
 			echo '<hr />';
 		}
+		
+		
 		
 		echo '<pre>';
 		print_r($htmlCounter);
@@ -134,7 +217,7 @@ foreach ($html->find('h1') as $link){
 		}
 		
 		echo '<br /> <strong>Other tests:</strong> <br /><br />';
-		
+
 		foreach ($nextBlock->find('a') as $innerLink){
 		if($innerLink->getAttribute('rel') != 'nofollow'){
 			if (($innerLink->href != '') && ($innerLink->title != '')) {
