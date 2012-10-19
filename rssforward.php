33ea590636f400ce47059399f69ba4ce6bed1125
Aram Zucker-Scharff <azuckers@gmu.edu>
2012-10-19 11:44:19 -0400

    Version update to 1.1.1

--- rssforward.php
+++ rssforward.php
@@ -3,7 +3,7 @@
 Plugin Name: RSS to PressForward
 Plugin URI: http://aramzs.me
 Description: This plugin is a RSS parser for CHNM's Press Forward project.
-Version: 1.1.0
+Version: 1.1.1
 Author: Aram Zucker-Scharff
 Author URI: http://aramzs.me
 License: GPL2
