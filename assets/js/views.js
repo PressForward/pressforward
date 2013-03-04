3a01b40c92939f75a4a343011274a48d9358c238
Aram Zucker-Scharff <azuckers@gmu.edu>
2013-03-04 13:18:00 -0500

    string assembly and placement for modal nav

--- assets/js/views.js
+++ assets/js/views.js
@@ -12,23 +12,41 @@ jQuery(document).ready(function() {
 		
 		//First lets assemble variables for the previous group.
 		if (jQuery(prevObj).is('*')){
-			var prevItemId = jQuery(prevObj).attr('id');
+			var prevItemID = jQuery(prevObj).attr('id');
 			var prevTitle = jQuery(prevObj).children('h1.item_title').text();
 			var prevSource = jQuery(prevObj).children('p.source_title').text();
 			var prevAuthor = jQuery(prevObj).children('span.item-authorship').text();
 			var prevExcerpt = jQuery(prevObj).children('div.item_excerpt').html();
 			var prevDate = jQuery(prevObj).children('p.pubdate').text();
+		
+			var prevHTML = '<h5 class="prev_title"><a href="#modal-'+prevItemID+'" role="button" data-toggle="modal" data-backdrop="false">'+prevTitle+'</a></h5>';
+			prevHTML += '<p class="prev_source_title">'+prevTitle+'</p>';
+			prevHTML += '<p class="prev_author">'+prevAuthor+'</p>';
+			prevHTML += '<p class="prev_excerpt">'+prevExcerpt+'</p>';
+			prevHTML += '<p class="prev_date">'+prevDate+'</p>';
+			
+			jQuery(currentObj).children('.modal .goPrev').html(prevHTML);	
+			
 		}
 		//Next lets assemble variables for the next group.
 		if (jQuery(nextObj).is('*')){
-			var nextItemId = jQuery(nextObj).attr('id');
+			var nextItemID = jQuery(nextObj).attr('id');
 			var nextTitle = jQuery(nextObj).children('h1.item_title').text();
 			var nextSource = jQuery(nextObj).children('p.source_title').text();
 			var nextAuthor = jQuery(nextObj).children('span.item-authorship').text();
 			var nextExcerpt = jQuery(nextObj).children('div.item_excerpt').html();
 			var nextDate = jQuery(nextObj).children('p.pubdate').text();
-		}
+
+			var nextHTML = '<h5 class="next_title"><a href="#modal-'+nextItemID+'" role="button" data-toggle="modal" data-backdrop="false">'+nextTitle+'</a></h5>';
+			nextHTML += '<p class="next_source_title">'+nextTitle+'</p>';
+			nextHTML += '<p class="next_author">'+nextAuthor+'</p>';
+			nextHTML += '<p class="next_excerpt">'+nextExcerpt+'</p>';
+			nextHTML += '<p class="next_date">'+nextDate+'</p>';
+			
+			jQuery(currentObj).children('.modal .goNext').html(nextHTML);			
 		
+		}
+				
 		
 	}
 	
