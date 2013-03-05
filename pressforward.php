38188f521bdaf39d426e619548ad55685f5f0542
Aram Zucker-Scharff <azuckers@gmu.edu>
2013-03-05 06:14:10 -0500

    working comments.
    Three features left to complete: star, archive, comment pop-up.

--- pressforward.php
+++ pressforward.php
@@ -3,7 +3,7 @@
 Plugin Name: PressForward
 Plugin URI: http://pressforward.org/
 Description: This plugin is a aggregation parser for CHNM's Press Forward project.
-Version: 1.8.2
+Version: 1.8.3
 Author: Aram Zucker-Scharff, Boone B Gorges, Jeremy Boggs
 Author URI: http://aramzs.me, http://boone.gorg.es/, http://clioweb.org
 License: GPL2
