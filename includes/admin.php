af84ac72079d934d3cf52c4bb751ef4f81d2a118
Aram Zucker-Scharff <azuckers@gmu.edu>
2013-03-27 16:06:40 -0400

    hiding mobile modal nav on desktops.

--- includes/admin.php
+++ includes/admin.php
@@ -366,7 +366,7 @@ class PF_Admin {
 			<div id="modal-<?php echo $item['item_id']; ?>" class="modal hide fade pfmodal" tabindex="-1" role="dialog" aria-labelledby="modal-<?php echo $item['item_id']; ?>-label" aria-hidden="true" pf-item-id="<?php echo $item['item_id']; ?>" pf-post-id="<?php echo $item['post_id']; ?>" pf-readability-status="<?php echo $item['readable_status']; ?>"> 
 			  <div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">x</button>				
-				<div class="modal-mobile-nav pull-right">
+				<div class="modal-mobile-nav pull-right hidden-desktop">
 					<div class="mobile-goPrev pull-left">
 					
 					</div>
