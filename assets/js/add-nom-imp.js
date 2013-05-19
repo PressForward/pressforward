a1e40cb5b8dc9de53b1de7dc4d5655b44184fad7
Aram Zucker-Scharff <azuckers@gmu.edu>
2013-05-19 19:31:44 -0400

    Removing the now (hopefully) unneeded closed
    Nomination info box now contains useful information and fields, so no
    need to close it.

--- assets/js/add-nom-imp.js
+++ assets/js/add-nom-imp.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
 
-	jQuery('#pf-nominations').addClass('closed');
+	//jQuery('#pf-nominations').addClass('closed');
 	jQuery('#publish').removeClass('button-primary').addClass('button').attr('value', 'Send to Draft');
 	jQuery('#save-post').addClass('button-primary').attr('value', 'Nominate');
 
