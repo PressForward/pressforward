f133ef822acdf98244340bd59d9ee8ae1d75c094
Aram Zucker-Scharff <azuckers@gmu.edu>
2013-10-14 00:39:26 -0400

    A full set of helper functions for creating, etc
    Especially useful for dealing with the transfer from the old to new
    feedlist.

--- includes/feeds.php
+++ includes/feeds.php
@@ -138,11 +138,78 @@ class PF_Feeds_Schema {
 	# Not only is this moving feeds over into feed CPT posts, but this methodology will insure a time-out won't force the process to restart.
 	# There should probably be a AJAX interface for this, same as the AB subscribe method. 
 	public function progressive_feedlist_transformer($feedlist, $xmlUrl, $key) {
-		$check = self::save_pf_feed($xmlUrl);
+		$check = self::create($xmlUrl, array('type' => 'rss-old'));
 		if ($check){
 			unset($feedlist[$key]);
 			update_option( PF_SLUG . '_feedlist', $feedlist );
 		}
+	}
+	
+	public function feed_post_setup($r, $insert_type = 'insert'){
+		
+		foreach ($r as $k=>$e){
+			if (!$e)
+				$r[$k] = '';
+		}
+		
+		$wp_args = array(
+			'post_type' 	=> $this->post_type,
+			'post_status' 	=> 'publish',
+			'post_title'	=> $r['title'],
+			'post_content'	=> $r['description'],
+			'tax_input' 	=> array($this->tag_taxonomy => $r['tags'])
+		);
+		
+		if ($insert_type == 'insert'){
+			$wp_args['guid'] = $r['url'];
+			$wp_args['post_date'] = date( 'Y-m-d H:i:s', time());
+			$post_id = wp_insert_post($wp_args);
+		} elseif ($insert_type == 'update') {
+			$wp_args['ID'] = $r['ID'];
+			wp_update_post( $wp_args );
+		}
+		if ( is_numeric($post_id) ){
+			self::set_pf_feed_type($post_id, $r['type']);
+			foreach ($r as $k=>$a){
+				if ($k == ('title'||'description'||'url'||'tags'||'type'))
+					unset($r[$k]);
+			}
+			self::set_feed_meta($post_id, $r);
+			return true;
+		} else {
+			return false;
+		}	
+	}
+	
+	public function setup_rss_meta($r, $theFeed){
+	
+		if(empty($r)){
+			
+		} else {
+			if (!$r['htmlUrl']){
+				$r['htmlUrl'] = $theFeed->get_permalink();
+			}
+			if (!$r['title']){
+				$r['title'] = $theFeed->get_title();
+			}
+			if (!$r['description']){
+				$r['description'] = $theFeed->get_description();
+			}
+			if (!$r['feed_icon']){
+				$r['feed_icon'] = $theFeed->get_favicon();
+			}	
+			if (!$r['feed_author']){
+				$r['feed_author'] = $theFeed->get_author();
+			}	
+			if (!$r['thumbnail']){
+				$r['thumbnail'] = $theFeed->get_image_url();
+			}				
+			if (empty($r['tags'])){
+				$r['tags'] = $theFeed->get_feed_tags();
+			}
+		}
+		return $r;
+	
 	}
 	
 	/*
@@ -171,62 +238,112 @@ class PF_Feeds_Schema {
 		
 			if (is_wp_error($theFeed = fetch_feed($feedURL))){
 				return new WP_Error('badfeed', __('The feed fails verification.'));
-			}
-			
-			if (!$r['htmlUrl']){
-				$r['htmlUrl'] = $theFeed->get_permalink();
-			}
-			if (!$r['title']){
-				$r['title'] = $theFeed->get_title();
-			}
-			if (!$r['description']){
-				$r['description'] = $theFeed->get_description();
-			}
-			if (!$r['feed_icon']){
-				$r['feed_icon'] = $theFeed->get_favicon();
-			}	
-			if (!$r['feed_author']){
-				$r['feed_author'] = $theFeed->get_author();
-			}	
-			if (!$r['thumbnail']){
-				$r['thumbnail'] = $theFeed->get_image_url();
-			}				
-			if (empty($r['tags'])){
-				$r['tags'] = $theFeed->get_feed_tags();
-			}			
+			} else {
+				$r = self::setup_rss_meta($r, $theFeed);
+			}		
 		}
 		if (!$r['user_added']){
 			$current_user = wp_get_current_user();
 			$r['user_added'] = $current_user->user_login;
 		}
-		
-		foreach ($r as $k=>$e){
-			if (!$e)
-				$r[$k] = '';
+		if ($r['type'] == 'rss-old'){
+			$r['title'] = $r['url'];
 		}
 		
+		self::feed_post_setup($r);
+		
+
+	}
+	
+	public function get( $args = array() ) {
 		$wp_args = array(
-			'post_type' 	=> $this->post_type,
-			'post_status' 	=> 'publish',
-			'post_title'	=> $r['title'],
-			'post_content'	=> $r['description'],
-			'guid'			=> $r['url'],
-			'post_date'		=> date( 'Y-m-d H:i:s', time());
-			'tax_input' 	=> array($this->tag_taxonomy => $r['tags'])
-		)
+			'post_type'        => $this->post_type,
+			'post_status'      => 'publish',
+			'suppress_filters' => false,
+		);
 
-		$post_id = wp_insert_post($wp_args);
-		if ( $post_id ){
-			self::set_pf_feed_type($post_id, $r['type']);
-			foreach ($r as $k=>$a){
-				if ($k == ('title'||'description'||'url'||'tags'||'type'))
-					unset($r[$k]);
+		$query_filters = array();
+
+		// WP_Query does not accept a 'guid' param, so we filter hackishly
+		if ( isset( $args['url'] ) ) {
+			$this->filter_data['guid'] = $args['url'];
+			unset( $args['url'] );
+			$query_filters['posts_where'][] = '_filter_where_guid';
+		}
+
+		foreach ( $query_filters as $hook => $filters ) {
+			foreach ( $filters as $f ) {
+				add_filter( $hook, array( $this, $f ) );
 			}
-			self::set_feed_meta($post_id, $r);
+		}
+
+		// Other WP_Query args pass through
+		$wp_args = wp_parse_args( $args, $wp_args );
+
+		$posts = get_posts( $wp_args );
+
+		foreach ( $query_filters as $hook => $filters ) {
+			foreach ( $filters as $f ) {
+				remove_filter( $hook, array( $this, $f ) );
+			}
+		}
+
+		// Fetch some handy pf-specific data
+
+
+		return $posts;
+	}	
+	
+	# When walking through the feedlist, if it is an old entry,
+	# call this function to renew the feed post with better data.
+	public function update_url($url){
+		$posts = self::get(array('url' => $url));
+		$c = 0;
+		foreach ($posts as $post){
+			setup_postdata($post);
+			$post_id = get_the_ID();
+			if ($c == 0){
+				self::update($post_id, array('url' => $url));
+			} else {
+				if ($url == get_the_guid($post_id)){
+					wp_delete_post( $post_id, true );
+				}
+			}
+			$c++;
+		}
+		wp_reset_postdata();
+	}
+	
+	public function update($post_id, $args){
+		$r = wp_parse_args( $args, array(
+			'ID'			=> $post_id,
+			'title'   		=> false,
+			'url'     		=> false,
+			'htmlUrl' 		=> false,
+			'type'	  		=> 'rss',
+			'description' 	=> false,
+			'feed_author' 	=> false,
+			'feed_icon'  	=> false,
+			'copyright'		=> false,			
+			'thumbnail'  	=> false,
+			'module_added' 	=> 'rss-import',
+			'tags'    		=> array(),
+		) );
+		if (!$r['url']){
+			return false;
+		}
+		if ($r['type'] == 'rss'){
 		
+			if (is_wp_error($theFeed = fetch_feed($feedURL))){
+				return new WP_Error('badfeed', __('The feed fails verification.'));
+			} else {
+				$r = self::setup_rss_meta($r, $theFeed);
+			}		
 		}
 		
+		self::feed_post_setup($r);
 
+		
 	}
 	
 	# This function makes it easy to set the type of 'feed', which is important when we move to using something other than RSS.
@@ -244,6 +361,7 @@ class PF_Feeds_Schema {
 	}
 	
 	public function set_feed_meta($post_id, $args){
+		$c = 1;
 		foreach ($args as $k=>$a){
 		
 			if(!$a){
@@ -251,7 +369,13 @@ class PF_Feeds_Schema {
 			} else {
 				update_post_meta($post_id, $k, $a);
 			}
+			$c++;
 		
+		}
+		
+		if ($c >= count($args)){
+			update_post_meta($post_id, 'meta_data', 'complete');
+
 		}
 	}
 	
