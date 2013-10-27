20936cc2eebb2249389462e3554485e8cc2678db
Aram Zucker-Scharff <azuckers@gmu.edu>
2013-10-27 14:13:27 -0400

    Setting up the first run at the new feed retrieval template.

---
+++ includes/slurp.php
@@ -0,0 +1,16 @@
+<?php 
+
+/**
+ * Feed 'slurping' class
+ *
+ * This class handles the functions for iterating through
+ * a feed list and retrieving the items in those feeds. 
+ * This class should only contain those functions that
+ * can be generalized to work on multiple content 
+ * retrieval methods (not just RSS).
+ *
+ */
+ 
+class PF_Feed_Retrieve {
+	
+}
