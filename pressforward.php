4fdaa41ffb34dd3300ea8e72c0f4cd68d50ac00d
Aram Zucker-Scharff <azuckers@gmu.edu>
2013-11-13 01:15:12 -0500

    Ver update to reflect new diagnostic nums #150
    View Log page now shows some of the diagnostic numbers requested in
    #150.

--- pressforward.php
+++ pressforward.php
@@ -3,7 +3,7 @@
 Plugin Name: PressForward
 Plugin URI: http://pressforward.org/
 Description: This plugin is a aggregation parser for CHNM's Press Forward project.
-Version: 2.1.4
+Version: 2.1.5
 Author: Aram Zucker-Scharff, Boone B Gorges, Jeremy Boggs
 Author URI: http://aramzs.me, http://boone.gorg.es/, http://clioweb.org
 License: GPL2
