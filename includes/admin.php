9520255e6087196b9eb368b5d2b855249f5ee2ad
Aram Zucker-Scharff <azuckers@gmu.edu>
2014-04-07 23:34:32 -0400

    Fixing count the posts function.

--- includes/admin.php
+++ includes/admin.php
@@ -1262,7 +1262,7 @@ class PF_Admin {
 	}
 
 	public function count_the_posts($post_type, $date_less = false){
-				
+
 				if (!$date_less){
 					$query_arg = array(
 						'post_type' 		=> $post_type,
@@ -1286,40 +1286,15 @@ class PF_Admin {
 						'posts_per_page' 	=> -1
 					);	
 				}
-				
+
 
 				$query = new WP_Query($query_arg);	
-		
-		return $query->post_count;
-	}
+				$post_count = $query->post_count;
+				wp_reset_postdata();
 
-	public function count_the_posts($post_type, $date_less = false){
-				
-				if (!$date_less){
-					$y = date('Y');
-					$m = date('m');
-				} elseif (!empty($date_less) && $date_less < 12) {
-					$y = date('Y');
-					$m = date('m');
-					$m = $m + $date_less;
-				} elseif (!empty($date_less) && $date_less >= 12) {
-					$y = date('Y');
-					$y = $y - floor($date_less/12);
-					$m = date('m');
-					$m = $m - (abs($date_less)-(12*floor($date_less/12)));
-				}
-				
-				$query_arg = array(
-					'post_type' 		=> $post_type,
-					'year'				=> $y,
-					'monthnum'			=> $m,
-					'posts_per_page' 	=> -1
-				);
-				$query = new WP_Query($query_arg);	
-		
-		return $query->post_count;
+		return $post_count;
 	}
-
+    
 	public function search_the_posts($s, $post_type){
 	
 		$args = array(
