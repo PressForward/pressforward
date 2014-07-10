cf92060d64f2b2d9ceb9702ba9da956ed4c73ca6
Boone B Gorges <boonebgorges@gmail.com>
2014-07-10 15:38:48 -0400

    User proper URL for registering Feed Tags subnav item
    This will allow the 'current' class to be added properly by WP's menu builder.
    
    See #308, #166

--- includes/admin.php
+++ includes/admin.php
@@ -102,9 +102,9 @@ class PF_Admin {
 		add_submenu_page(
 			PF_MENU_SLUG, 
 			__('Feed Tags', 'pf'),
-			__('Feed Tags', 'pf'), 
-			get_option('pf_menu_feeder_access', pf_get_defining_capability_by_role('editor')), 
-			'edit-tags.php?taxonomy=' . pressforward()->pf_feeds->tag_taxonomy . '&post_type='.pressforward()->pf_feeds->post_type . '&top_menu=' . PF_MENU_SLUG,
+			__('Feed Tags', 'pf'),
+			get_option('pf_menu_feeder_access', pf_get_defining_capability_by_role('editor')),
+			'edit-tags.php?taxonomy=' . pressforward()->pf_feeds->tag_taxonomy,
 			''
 		);			
 /**
