6bd1d8f049d9218a44214a71ff93934d3551d755
Boone B Gorges <boonebgorges@gmail.com>
2014-10-16 13:13:29 -0400

    Don't prime post term cache when fetching parents.
    See #38.

--- includes/feed-items.php
+++ includes/feed-items.php
@@ -228,18 +228,19 @@ class PF_Feed_Item {
 		if (isset($_GET['feed'])) {
 			$post_args['post_parent'] = $_GET['feed'];
 		} elseif (isset($_GET['folder'])){
-			$parents_in_folder = new WP_Query(array(
-					'post_type'								=> pressforward()->pf_feeds->post_type,
-					'fields'									=> 'ids',
-					'update_post_meta_cache' 	=> false,
-					'tax_query'								=> array(
-														array(
-															'taxonomy'	=> pressforward()->pf_feeds->tag_taxonomy,
-															'field'			=> 'term_id',
-															'terms'			=> $_GET['folder']
-														)
-					)
-			));
+			$parents_in_folder = new WP_Query( array(
+				'post_type' => pressforward()->pf_feeds->post_type,
+				'fields'=> 'ids',
+				'update_post_term_cache' => false,
+				'update_post_meta_cache' => false,
+				'tax_query' => array(
+					array(
+						'taxonomy' => pressforward()->pf_feeds->tag_taxonomy,
+						'field'	=> 'term_id',
+						'terms'	=> $_GET['folder']
+					),
+				),
+			) );
 			#var_dump('<pre>'); var_dump($parents_in_folder); die();
 			$post_args['post_parent__in'] = $parents_in_folder->posts;
 		}
