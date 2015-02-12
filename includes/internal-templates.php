def54972f282cf773ee0d6afcd42be2cebf14da5
Aram Zucker-Scharff <azuckers@gmu.edu>
2015-02-11 21:30:16 -0500

    Move menu building process to stand alone file
    And formalize the process for building such files.

--- includes/internal-templates.php
+++ includes/internal-templates.php
@@ -13,8 +13,67 @@ class PF_Form_Of {
 	}
 
 	public function init() {
-		$this->parts = PF_ROOT . "/parts/";
+		$this->parts = $this->build_path(array(PF_ROOT, "parts"), false);
 		$this->the_screen = $this->the_screen();
+	}
+
+	/**
+	 * Build file paths.
+	 *
+	 * Build paths with arrays Call out of static function class_name->build_path
+	 * or self::build_path. Use like:
+	 *
+	 * 		build_path(array("home", "alice", "Documents", "example.txt"));
+	 *
+	 * @see http://php.net/manual/en/dir.constants.php
+	 * @global string DIRECTORY_SEPARATOR Called from class definition, system variable
+	 *
+	 * @param array $segments The pieces of the URL, should be array of strings. Default null Accepts string.
+	 * @param bool $leading Optional If the returned path should have a leading slash. Default true.
+	 * @param bool $url Optional If the returned path should use web URL style pathing or system style. Default false
+	 * @return string The composed path.
+	 *
+	 */
+	public function build_path($segments=array(), $leading = true, $url = false) {
+		if ($url){
+            $slash = '/';
+        } else {
+            $slash = DIRECTORY_SEPARATOR;
+        }
+        $string = join($slash, $segments);
+		if ($leading){
+			$string = $slash . $string;
+		}
+		return $string;
+	}
+
+	/**
+	 * Get a given view (if it exists)
+	 * 
+	 * @param string     $view      The slug of the view
+	 * @return string
+	 */
+	public function get_view( $view, $vars = array() ) {
+		$view_file = $this->build_path(array($this->parts, $view.'.tpl.php'), false);
+		#if (WP_DEBUG){ var_dump( $view_file ); }
+		if ( ! file_exists( $view_file ) ){
+			if (WP_DEBUG){ var_dump( $view_file ); }
+			return ' ';
+		}
+		extract( $vars, EXTR_SKIP );
+		ob_start();
+		include $view_file;
+		return ob_get_clean();
+	}
+
+	public function the_side_menu(){
+		$screen = $this->the_screen;
+		$vars = array(
+				'slug'		=> $screen['id'],
+				'version'	=> 0,
+				'deck'		=> false
+			);
+		echo $this->get_view('side-menu', $vars);
 	}
 
 	public function valid_pf_page_ids($page_id = false){
