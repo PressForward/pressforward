bca70f2e91423a778837c82726a20ae6af0d3272
Douglas Linsmeyer <dlinsmey@nerdery.com>
2015-02-11 22:16:58 -0700

    Adds post type and post status configuration
    - Adds ability to designate post type of new content
    - Adds ability to designate post status of new content

--- includes/admin.php
+++ includes/admin.php
@@ -841,7 +841,7 @@ class PF_Admin {
 	 */
 	function display_review_builder() {
 		include( PF_ROOT . "/includes/under-review/under-review.php" );
-		
+
 	}
 
 	function ajax_get_comments(){
@@ -967,7 +967,30 @@ class PF_Admin {
 
 					echo '<label class="description" for="pf_errors_until_alert"> ' .__('Number of errors before a feed is marked as malfunctioning.', 'pf'). ' </label>';
 					?></p>
-					<br />
+
+					<p>
+						<select name="<?php echo PF_SLUG; ?>_draft_post_status" id="<?php echo PF_SLUG; ?>_draft_post_status"><?php
+							$post_statuses = get_post_statuses();
+							$pf_draft_post_status_value = get_option(PF_SLUG.'_draft_post_status', 'draft');
+							foreach ($post_statuses as $status_name => $status_label): ?>
+								<option value="<?php echo $status_name; ?>" <?php if ($pf_draft_post_status_value === $status_name) echo 'selected="selected"'; ?>><?php echo $status_label; ?></option><?php
+							endforeach; ?>
+						</select>
+						<label class="description" for="<?php echo PF_SLUG; ?>_draft_post_status"><?php echo __('Post status for new content.', 'pf'); ?></label>
+					</p>
+
+					<p>
+						<select name="<?php echo PF_SLUG; ?>_draft_post_type" id="<?php echo PF_SLUG; ?>_draft_post_type"><?php
+							$post_types = get_post_types(array('public' => true), 'objects');
+							$pf_draft_post_type_value = get_option(PF_SLUG.'_draft_post_type', 'post');
+							foreach ($post_types as $post_type): ?>
+								<option value="<?php echo $post_type->name; ?>" <?php if ($pf_draft_post_type_value === $post_type->name) echo 'selected="selected"'; ?>><?php echo $post_type->label; ?></option><?php
+							endforeach; ?>
+						</select>
+						<label class="description" for="<?php echo PF_SLUG; ?>_draft_post_type"><?php echo __('Post type for new content.', 'pf'); ?></label>
+					</p>
+
+				<br />
 
 					<input type="submit" name="submit" class="button-primary" value="<?php _e( "Save Changes", 'pf' ) ?>" />
 					<br />
@@ -1391,6 +1414,15 @@ class PF_Admin {
 			update_option('pf_errors_until_alert', 3);
 		}
 
+		$pf_draft_post_type = (isset( $_POST[PF_SLUG . '_draft_post_type'] ))
+			? $_POST[PF_SLUG . '_draft_post_type']
+			: 'post';
+		update_option(PF_SLUG . '_draft_post_type', $pf_draft_post_type);
+
+		$pf_draft_post_status = (isset( $_POST[PF_SLUG . '_draft_post_status'] ))
+			? $_POST[PF_SLUG . '_draft_post_status']
+			: 'draft';
+		update_option(PF_SLUG . '_draft_post_status', $pf_draft_post_status);
 
 		if (isset( $_POST['pf_present_author_as_primary'] )){
 			$pf_author_opt_check = $_POST['pf_present_author_as_primary'];
