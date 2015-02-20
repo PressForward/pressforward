3ed3c50984c7bc7e62342eedd1f15ec2c7902549
Aram Zucker-Scharff <azuckers@gmu.edu>
2015-02-20 15:13:21 -0500

    Testing clears

--- modules/rss-import/rss-import.php
+++ modules/rss-import/rss-import.php
@@ -287,7 +287,9 @@ class PF_RSS_Import extends PF_Module {
                         <input type="submit" class="button-primary" value="<?php _e('Save Options', 'pf'); ?>" />
                     </div>
             </div>
+            <div class="clear"></div>
 		</div>
+		<div class="clear"></div>
 		<?php
 
 
