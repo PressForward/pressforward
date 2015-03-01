727be8e240fd99e16b6d4fc9f4841aaa2d5acb77
Aram Zucker-Scharff <azuckers@gmu.edu>
2015-03-01 12:59:13 -0500

    Why was this ever a class in the first place? ID now.
    Properly detect a click of the archive visibility toggle. Resolves #560
    and #536

--- assets/js/nom-archive-imp.js
+++ assets/js/nom-archive-imp.js
@@ -21,21 +21,21 @@ var archivesVisible = 0;
 
 jQuery(window).load(function() {
 
-	jQuery(".showarchived").click().toggle(function (evt){ 
+	jQuery("#showarchived").click().toggle(function (evt){ 
 		evt.preventDefault();
 		archivesVisible = 1;
 		jQuery('.archived').show();
-		jQuery('.showarchived').html('Hide Archived');
-		jQuery('.showarchived').attr('value', 'Hide Archived');
-		jQuery('.showarchived').addClass('hidearchived');
+		jQuery('#showarchived').html('Hide Archived');
+		jQuery('#showarchived').attr('value', 'Hide Archived');
+		jQuery('#showarchived').addClass('hidearchived');
 	
 	}, function (evt){ 
 		evt.preventDefault();
 		archivesVisible = 0;
 		jQuery('.archived').hide();
-		jQuery('.hidearchived').html('Show Archived');
-		jQuery('.hidearchived').attr('value', 'Show Archived');
-		jQuery('.showarchived').removeClass('hidearchived');
+		jQuery('#hidearchived').html('Show Archived');
+		jQuery('#hidearchived').attr('value', 'Show Archived');
+		jQuery('#showarchived').removeClass('hidearchived');
 	
 	}
 	);		
