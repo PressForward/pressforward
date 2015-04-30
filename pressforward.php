bcea3ecd8c900ad0190aadf83ba7930b5d9a6815
Aram Zucker-Scharff <azuckers@gmu.edu>
2015-04-29 21:20:54 -0400

    Updating version number

--- pressforward.php
+++ pressforward.php
@@ -3,11 +3,7 @@
 Plugin Name: PressForward
 Plugin URI: http://pressforward.org/
 Description: The PressForward Plugin is a tool by the Roy Rosenzweig Center for History and New Media for aggregating and curating web-based content within the WordPress dashboard.
-<<<<<<< HEAD
-Version: 3.6.0
-=======
-Version: 3.5.2
->>>>>>> master
+Version: 3.6.1
 GitHub Plugin URI: https://github.com/PressForward/pressforward
 Author: Aram Zucker-Scharff, Boone B Gorges, Jeremy Boggs
 Author URI: http://pressforward.org/about/team/
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link:
 Tags: aggregate, aggregation, aggregator, atom, attribution, circulate, collect, community, content curation, curate, curation, curation tool, discuss, distribute, editorial, feed, network, news, opml, OPML, read, reader, reblog, reblogging, republish, review, RSS, rss, share, syndicate, syndication, workflow
 Requires at least: 3.0.1
 Tested up to: 4.2
-Stable tag: 3.5.2
+Stable tag: 3.6.1
 License: GPL2
 License URI: http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 
