f5cb1fedeebe7de52dd5e6c5838bbf4cd2f0c959
Aram Zucker-Scharff <azuckers@gmu.edu>
2015-07-20 22:59:07 -0400

    Gulpfile build and some setup files.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,19 @@
+'use strict';
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'));
+gulp.task('sass', function() {
+    return gulp.src('assets/sass/**/*.scss')
+    	.pipe(sass())
+        .on('error', sass.logError)
+        .pipe(gulp.dest('assets/css/'))
+        .on('error', function(error) {
+        	console.log(error);
+		});
 });
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+    gulp.watch('assets/sass/**/*.scss',['sass']);
 });
