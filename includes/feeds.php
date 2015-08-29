677b38a4f87cc03457b81b5aceddffb548418ffc
Aram Zucker-Scharff <azuckers@gmu.edu>
2015-08-29 14:54:10 -0400

    Show feed URL in Subscribed Posts list
    Resolves #584

--- includes/feeds.php
+++ includes/feeds.php
@@ -49,6 +49,7 @@ class PF_Feeds_Schema {
 			add_action('wp_ajax_deal_with_old_feedlists', array($this, 'deal_with_old_feedlists'));
 			add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
 			add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_edit_feed_scripts' ) );
+			add_filter( 'page_row_actions', array($this, 'url_feed_row_action'), 10, 2 );
 		}
 
 		add_filter('manage_edit-'.$this->post_type.'_columns', array( $this, 'custom_feed_column_name'));
@@ -102,6 +103,19 @@ class PF_Feeds_Schema {
 		} else {
 			return true;
 		}
+	}
+
+	public function url_feed_row_action( $actions, $post ) {
+	    if ( $post->post_type != $this->post_type ) {
+	        return $actions;
+			#var_dump($actions); die();
+	    }
+
+		$url = $post->guid;
+		$actions['edit'] = '<span class="inline" style="visibility:visible;color:grey;">'.$url.'</span><br/>'.$actions['edit'];
+		//<span class="inline hide-if-no-js"><a href="#" class="editinline" title="Edit this item inline">Quick&nbsp;Edit</a> | </span>
+	    #$actions['wpse8170-pdf'] = 'http://your/url/here';
+	    return $actions;
 	}
 
 	public function get_top_feed_folders(){
