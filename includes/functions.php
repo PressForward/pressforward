a828ec9aa1519429ce783e1e33851d34198f1333
Aram Zucker-Scharff <azuckers@gmu.edu>
2015-10-17 13:55:59 -0400

    Not all browsers respect this, but NomThis height should be 620
    This matches actual body height.

--- includes/functions.php
+++ includes/functions.php
@@ -101,7 +101,7 @@ function pf_shortcut_link() {
 				l=d.location,
 				e=encodeURIComponent,
 				u=f+'?u='+e(l.href)+'&t='+e(d.title)+'&s='+e(s)+'&v=4';
-				a=function(){if(!w.open(u,'t','toolbar=0,resizable=1,scrollbars=1,status=1,width=720,height=570'))l.href=u;};
+				a=function(){if(!w.open(u,'t','toolbar=0,resizable=1,scrollbars=1,status=1,width=720,height=620'))l.href=u;};
 				if (/Firefox/.test(navigator.userAgent)) setTimeout(a, 0); else a();
 				void(0)";
 
