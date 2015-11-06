b0a474ac0a9048b5999c93d2d0431ba035578980
Aram Zucker-Scharff <azuckers@gmu.edu>
2015-11-05 23:07:39 -0500

    Reverting what is hopefully the cause of the feed fails

--- includes/slurp.php
+++ includes/slurp.php
@@ -358,7 +358,6 @@ class PF_Feed_Retrieve {
 
 		if ( !isset( $theFeeds ) ) {
 			# @todo a better error report
-			pf_log('No Feedlist for some reason.');
 			return false;
 		} elseif ( is_wp_error( $theFeeds ) ) {
 			return $theFeeds;
@@ -518,6 +517,16 @@ class PF_Feed_Retrieve {
 			die();
 		}
 
+		if ( ( 'rss-quick' == $type ) && ( 'publish' == $obj->post_status ) ) {
+			# Let's update the RSS-Quick so it has real data.
+			$rq_update = array(
+				'type'		=>		'rss-quick',
+				'ID'		=>		$id,
+				'url'		=>		$obj->guid
+			);
+			$Feeds->update( $id, $rq_update );
+		}
+
 		# module function to return a set of standard pf feed_item object
 		# Like get_items in SimplePie
 		$feedObj = $this->get_the_feed_object( $module_to_use, $obj );
@@ -562,16 +571,6 @@ class PF_Feed_Retrieve {
 		            );
 		            $result = wp_update_post($argup);
 				}
-			}
-
-			if ( ( 'rss-quick' == $type ) && ( 'publish' == $obj->post_status ) ) {
-				# Let's update the RSS-Quick so it has real data.
-				$rq_update = array(
-					'type'		=>		'rss-quick',
-					'ID'		=>		$id,
-					'url'		=>		$obj->guid
-				);
-				$Feeds->update( $id, $rq_update );
 			}
 
 			return $feedObj;
