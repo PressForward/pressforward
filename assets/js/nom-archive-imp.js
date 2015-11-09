ddca4c53cc1b2f01899f7d0bb567b2ec8c869588
Aram Zucker-Scharff <azuckers@gmu.edu>
2015-11-08 23:24:28 -0500

    Make archive button work more universally and with greater severity
    These changes will make clicking the Archive button in Under Review
    apply to all users and will additionally archive feed items for all
    users as well. The reverse will also be true when clicked to
    un-archive.
    
    Resolves #406 and #427

--- assets/js/nom-archive-imp.js
+++ assets/js/nom-archive-imp.js
@@ -4,7 +4,7 @@
 jQuery(window).load(function() {
 
 	jQuery('.pf_container').on('click', ".nom-to-archive", function (evt){
-		evt.preventDefault();
+		//evt.preventDefault();
 
 	var element			= jQuery(this);
 	var nomID			= element.attr('form');
@@ -13,6 +13,8 @@ jQuery(window).load(function() {
 			jQuery('#'+nomID).addClass('archived');
 			if (archivesVisible == 0){
 				jQuery('#'+nomID).hide();
+			} else {
+				jQuery('#'+nomID).removeClass('archived');
 			}
 	});
 });
@@ -70,6 +72,78 @@ jQuery(window).load(function() {
 			jQuery('#errors').html(response);
 		});
 	});
+
+	jQuery('.pf_container').on('click', ".not-archived .nom-to-archive", function (evt){
+		evt.preventDefault();
+		console.log('Move to archive.');
+		var element		= jQuery(this);
+		var itemID		= element.attr('form');
+	var item_title 		= jQuery("#item_title_"+itemID).val();
+	var source_title 	= jQuery("#source_title_"+itemID).val();
+	var item_date 		= jQuery("#item_date_"+itemID).val();
+	var item_author 	= jQuery("#item_author_"+itemID).val();
+	var item_content 	= jQuery("#item_content_"+itemID).val();
+	var item_link 		= jQuery("#item_link_"+itemID).val();
+	var item_feat_img 	= jQuery("#item_feat_img_"+itemID).val();
+	var item_id 		= jQuery("#item_id_"+itemID).val();
+	var item_wp_date	= jQuery("#item_wp_date_"+itemID).val();
+	var item_tags		= jQuery("#item_tags_"+itemID).val();
+	var source_repeat	= jQuery("#source_repeat_"+itemID).val();
+	var postID 			= jQuery('#'+itemID).attr('pf-item-post-id');
+	var nomID 			= jQuery('#'+itemID).attr('pf-post-id');
+//	var errorThrown		= 'Broken';
+	var theNonce		= jQuery.trim(jQuery('#pf_nomination_nonce').val());
+	jQuery('.loading-'+itemID).show();
+	jQuery.post(ajaxurl, {
+			action: 'pf_ajax_move_to_archive',
+			item_post_id: postID,
+			nom_id: nomID,
+			source_repeat: source_repeat,
+			pf_nomination_nonce: theNonce
+		},
+		function(response) {
+			jQuery('.loading-'+itemID).hide();
+			//jQuery(".nominate-result-"+itemID).html(response);
+			//alert(response);
+			//jQuery("#test-div1").append(data);
+		});
+	  });
+
+	jQuery('.pf_container').on('click', ".archived .nom-to-archive", function (evt){
+		evt.preventDefault();
+		console.log('Move out of archive.');
+		var element		= jQuery(this);
+		var itemID		= element.attr('form');
+	var item_title 		= jQuery("#item_title_"+itemID).val();
+	var source_title 	= jQuery("#source_title_"+itemID).val();
+	var item_date 		= jQuery("#item_date_"+itemID).val();
+	var item_author 	= jQuery("#item_author_"+itemID).val();
+	var item_content 	= jQuery("#item_content_"+itemID).val();
+	var item_link 		= jQuery("#item_link_"+itemID).val();
+	var item_feat_img 	= jQuery("#item_feat_img_"+itemID).val();
+	var item_id 		= jQuery("#item_id_"+itemID).val();
+	var item_wp_date	= jQuery("#item_wp_date_"+itemID).val();
+	var item_tags		= jQuery("#item_tags_"+itemID).val();
+	var source_repeat	= jQuery("#source_repeat_"+itemID).val();
+	var postID 			= jQuery('#'+itemID).attr('pf-item-post-id');
+	var nomID 			= jQuery('#'+itemID).attr('pf-post-id');
+//	var errorThrown		= 'Broken';
+	var theNonce		= jQuery.trim(jQuery('#pf_nomination_nonce').val());
+	jQuery('.loading-'+itemID).show();
+	jQuery.post(ajaxurl, {
+			action: 'pf_ajax_move_out_of_archive',
+			item_post_id: postID,
+			nom_id: nomID,
+			source_repeat: source_repeat,
+			pf_nomination_nonce: theNonce
+		},
+		function(response) {
+			jQuery('.loading-'+itemID).hide();
+			//jQuery(".nominate-result-"+itemID).html(response);
+			//alert(response);
+			//jQuery("#test-div1").append(data);
+		});
+	  });
 
 
 });
