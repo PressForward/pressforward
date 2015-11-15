6a44898812686ae969ee36081025b6363ec36c24
Amanda Regan <regan008@cougars.csusm.edu>
2015-11-15 14:32:47 -0500

    Fixing typo in clean up button text.

--- parts/pf-tools/tab-reset-refresh.tpl.php
+++ parts/pf-tools/tab-reset-refresh.tpl.php
@@ -13,7 +13,7 @@
 		<p>
 			<button type="submit" class="cleanfeeds btn btn-small" id="cleanfeeds" value="<?php  _e('Clean Up', 'pf')  ?>"><?php  _e('Clean Up', 'pf');  ?></button>
 			<?php
-				_e( ' the feed items. You can press this button this button to manually initiate the process of selecting feed items more than 2 months old. There is a chance of initiating this process simultaneously with a process triggered automatically every 30 minutes. If this occurs, error messages will likely appear in your server logs.', 'pf');
+				_e( ' the feed items. You can press this button to manually initiate the process of selecting feed items more than 2 months old. There is a chance of initiating this process simultaneously with a process triggered automatically every 30 minutes. If this occurs, error messages will likely appear in your server logs.', 'pf');
 			?>
 		</p>
 		<?php
