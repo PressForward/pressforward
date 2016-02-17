39ff937a873bad19776e55b91e8438b11714f7a5
Aram Zucker-Scharff <azuckers@gmu.edu>
2016-02-16 23:47:22 -0500

    Improving the interface for System_Items

--- interfaces/interface-items.php
+++ interfaces/interface-items.php
@@ -3,7 +3,7 @@ interface System_Items
 {
     public function insert_post( $post, $error );
     public function update_post( $post, $error );
-	public function delete_post( $postid, $force_delete );
+	public function delete_post( $postid, $force_delete = false );
 	public function get_post(  $post = null, $output = OBJECT, $filter = 'raw' );
 	public function is_error( $post );
 }
