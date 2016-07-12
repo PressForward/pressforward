33836c00a008a685549efdd894fcc737e1872610
Aram Zucker-Scharff <azuckers@gmu.edu>
2016-07-11 23:22:32 -0400

    Update OpenGraph library
    New opengraph library is much more versatile in finding the image on
    page. This should resolve #731 to a reasonable level of satisfaction.

--- Libraries/PFOpenGraph.php
+++ Libraries/PFOpenGraph.php
@@ -20,186 +20,236 @@
 
 class PFOpenGraph implements Iterator
 {
-  /**
-   * There are base schema's based on type, this is just
-   * a map so that the schema can be obtained
-   *
-   */
-	public static $TYPES = array(
-		'activity' => array('activity', 'sport'),
-		'business' => array('bar', 'company', 'cafe', 'hotel', 'restaurant'),
-		'group' => array('cause', 'sports_league', 'sports_team'),
-		'organization' => array('band', 'government', 'non_profit', 'school', 'university'),
-		'person' => array('actor', 'athlete', 'author', 'director', 'musician', 'politician', 'public_figure'),
-		'place' => array('city', 'country', 'landmark', 'state_province'),
-		'product' => array('album', 'book', 'drink', 'food', 'game', 'movie', 'product', 'song', 'tv_show'),
-		'website' => array('blog', 'website'),
-	);
+	/**
+     * There are base schema's based on type, this is just
+     * a map so that the schema can be obtained
+     *
+     */
+  	public static $TYPES = array(
+  		'activity' => array('activity', 'sport'),
+  		'business' => array('bar', 'company', 'cafe', 'hotel', 'restaurant'),
+  		'group' => array('cause', 'sports_league', 'sports_team'),
+  		'organization' => array('band', 'government', 'non_profit', 'school', 'university'),
+  		'person' => array('actor', 'athlete', 'author', 'director', 'musician', 'politician', 'public_figure'),
+  		'place' => array('city', 'country', 'landmark', 'state_province'),
+  		'product' => array('album', 'book', 'drink', 'food', 'game', 'movie', 'product', 'song', 'tv_show'),
+  		'website' => array('blog', 'website'),
+  	);
 
-  /**
-   * Holds all the Open Graph values we've parsed from a page
-   *
-   */
-	private $_values = array();
+    /**
+     * Holds all the Open Graph values we've parsed from a page
+     *
+     */
+  	private $_values = array();
 
-  /**
-   * Fetches a URI and parses it for Open Graph data, returns
-   * false on error.
-   *
-   * @param $URI    URI to page to parse for Open Graph data
-   * @return OpenGraph
-   */
-	static public function fetch($URI) {
-		$URI_data = pf_de_https($URI, 'wp_remote_get', array('timeout' => '30'));
-    return self::_parse($URI_data);
-	}
+    /**
+     * Fetches a URI and parses it for Open Graph data, returns
+     * false on error.
+     *
+     * @param $URI    URI to page to parse for Open Graph data
+     * @return OpenGraph
+     */
+  	static public function fetch($URI) {
+      $cookie_path = 'cookie.txt';
+      if ( defined('COOKIE_PATH_FOR_CURL') && !empty(COOKIE_PATH_FOR_CURL) ){
+        $cookie_path = COOKIE_PATH_FOR_CURL;
+      }
+      $curl = curl_init($URI);
 
-  /**
-   * Parses HTML and extracts Open Graph data, this assumes
-   * the document is at least well formed.
-   *
-   * @param $HTML    HTML to parse
-   * @return OpenGraph
-   */
-	static private function _parse($HTML) {
-		if (empty($HTML)){ return false; }
-		$old_libxml_error = libxml_use_internal_errors(true);
+      curl_setopt($curl, CURLOPT_FAILONERROR, true);
+      curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($curl, CURLOPT_TIMEOUT, 15);
+      curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+      curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+      curl_setopt($curl, CURLOPT_USERAGENT, "facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)");
+      //The following 2 set up lines work with sites like www.nytimes.com
+      curl_setopt($curl, CURLOPT_COOKIEFILE, $cookie_path); //you can change this path to whetever you want.
+      curl_setopt($curl, CURLOPT_COOKIEJAR, $cookie_path); //you can change this path to whetever you want.
 
-		$doc = new \DOMDocument();
-		if (is_array($HTML)){
-			pf_log('Attempt to parse URL for OpenGraph.');
-			#pf_log($HTML);
-			$HTML = $HTML['body'];
-			if (empty($HTML)){
-				return false;
-			}
-		}
-		$doc->loadHTML($HTML);
+      $response = mb_convert_encoding(curl_exec($curl), 'HTML-ENTITIES', 'UTF-8');
 
-		libxml_use_internal_errors($old_libxml_error);
+      curl_close($curl);
 
-		$tags = $doc->getElementsByTagName('meta');
-		if (!$tags || $tags->length === 0) {
-			return false;
-		}
+      if (!empty($response)) {
+          return self::_parse($response);
+      } else {
+          return false;
+      }
+  	}
 
-		$page = new self();
+    /**
+     * Parses HTML and extracts Open Graph data, this assumes
+     * the document is at least well formed.
+     *
+     * @param $HTML    HTML to parse
+     * @return OpenGraph
+     */
+  	static private function _parse($HTML) {
+  		$old_libxml_error = libxml_use_internal_errors(true);
 
-		$nonOgDescription = null;
+  		$doc = new DOMDocument();
+  		$doc->loadHTML($HTML);
 
-		foreach ($tags AS $tag) {
-			if ($tag->hasAttribute('property') &&
-			    strpos($tag->getAttribute('property'), 'og:') === 0) {
-				$key = strtr(substr($tag->getAttribute('property'), 3), '-', '_');
-				$page->_values[$key] = $tag->getAttribute('content');
-			}
+  		libxml_use_internal_errors($old_libxml_error);
 
-			//Added this if loop to retrieve description values from sites like the New York Times who have malformed it.
-			if ($tag ->hasAttribute('value') && $tag->hasAttribute('property') &&
-			    strpos($tag->getAttribute('property'), 'og:') === 0) {
-				$key = strtr(substr($tag->getAttribute('property'), 3), '-', '_');
-				$page->_values[$key] = $tag->getAttribute('value');
-			}
-			//Based on modifications at https://github.com/bashofmann/opengraph/blob/master/src/OpenGraph/OpenGraph.php
-			if ($tag->hasAttribute('name') && $tag->getAttribute('name') === 'description') {
-                $nonOgDescription = $tag->getAttribute('content');
-            }
+  		$tags = $doc->getElementsByTagName('meta');
+  		if (!$tags || $tags->length === 0) {
+  			return false;
+  		}
 
-		}
-		//Based on modifications at https://github.com/bashofmann/opengraph/blob/master/src/OpenGraph/OpenGraph.php
-		if (!isset($page->_values['title'])) {
-            $titles = $doc->getElementsByTagName('title');
-            if ($titles->length > 0) {
-                $page->_values['title'] = $titles->item(0)->textContent;
-            }
-        }
-        if (!isset($page->_values['description']) && $nonOgDescription) {
-            $page->_values['description'] = $nonOgDescription;
-        }
+  		$page = new self();
 
-        //Fallback to use image_src if ogp::image isn't set.
-        if (!isset($page->_values['image'])) {
-            $domxpath = new DOMXPath($doc);
-            $elements = $domxpath->query("//link[@rel='image_src']");
+  		$nonOgDescription = null;
 
-            if ($elements->length > 0) {
-                $domattr = $elements->item(0)->attributes->getNamedItem('href');
-                if ($domattr) {
-                    $page->_values['image'] = $domattr->value;
-                    $page->_values['image_src'] = $domattr->value;
-                }
-            }
-        }
+  		foreach ($tags AS $tag) {
+  			if ($tag->hasAttribute('property') && strpos($tag->getAttribute('property'), 'og:') === 0) {
+  				$key = strtr(substr($tag->getAttribute('property'), 3), '-', '_');
 
-		if (empty($page->_values)) { return false; }
+  		        if( array_key_exists($key, $page->_values) ){
+  					if ( !array_key_exists($key.'_additional', $page->_values) ){
+  						$page->_values[$key.'_additional'] = array();
+  					}
+  		        	$page->_values[$key.'_additional'][] = $tag->getAttribute('content');
+  		        }else{
+  		        	$page->_values[$key] = $tag->getAttribute('content');
+  		        }
+  			}
 
-		return $page;
-	}
+  			//Added this if loop to retrieve description values from sites like the New York Times who have malformed it.
+  			if ($tag->hasAttribute('value') && $tag->hasAttribute('property') &&
+  			    strpos($tag->getAttribute('property'), 'og:') === 0) {
+  				$key = strtr(substr($tag->getAttribute('property'), 3), '-', '_');
+  				$page->_values[$key] = $tag->getAttribute('value');
+  			}
+  			//Based on modifications at https://github.com/bashofmann/opengraph/blob/master/src/OpenGraph/OpenGraph.php
+  			if ($tag->hasAttribute('name') && $tag->getAttribute('name') === 'description') {
+                  $nonOgDescription = $tag->getAttribute('content');
+              }
 
-  /**
-   * Helper method to access attributes directly
-   * Example:
-   * $graph->title
-   *
-   * @param $key    Key to fetch from the lookup
-   */
-	public function __get($key) {
-		if (array_key_exists($key, $this->_values)) {
-			return $this->_values[$key];
-		}
+  			if ($tag->hasAttribute('property') &&
+  			    strpos($tag->getAttribute('property'), 'twitter:') === 0) {
+  				$key = strtr($tag->getAttribute('property'), '-:', '__');
+  				$page->_values[$key] = $tag->getAttribute('content');
+  			}
 
-		if ($key === 'schema') {
-			foreach (self::$TYPES AS $schema => $types) {
-				if (array_search($this->_values['type'], $types)) {
-					return $schema;
-				}
-			}
-		}
-	}
+  			if ($tag->hasAttribute('name') &&
+  				strpos($tag->getAttribute('name'), 'twitter:') === 0) {
+  				$key = strtr($tag->getAttribute('name'), '-:', '__');
+  				if( array_key_exists($key, $page->_values) ){
+  					if (!array_key_exists($key.'_additional', $page->_values)){
+  						$page->_values[$key.'_additional'] = array();
+  					}
+  					$page->_values[$key.'_additional'][] = $tag->getAttribute('content');
+  				} else {
+  					$page->_values[$key] = $tag->getAttribute('content');
+  				}
+  			}
+  		}
+  		//Based on modifications at https://github.com/bashofmann/opengraph/blob/master/src/OpenGraph/OpenGraph.php
+  		if (!isset($page->_values['title'])) {
+              $titles = $doc->getElementsByTagName('title');
+              if ($titles->length > 0) {
+                  $page->_values['title'] = $titles->item(0)->textContent;
+              }
+          }
+          if (!isset($page->_values['description']) && $nonOgDescription) {
+              $page->_values['description'] = $nonOgDescription;
+          }
 
-  /**
-   * Return all the keys found on the page
-   *
-   * @return array
-   */
-	public function keys() {
-		return array_keys($this->_values);
-	}
+          //Fallback to use image_src if ogp::image isn't set.
+          if (!isset($page->_values['image'])) {
+              $domxpath = new DOMXPath($doc);
+              $elements = $domxpath->query("//link[@rel='image_src']");
 
-  /**
-   * Helper method to check an attribute exists
-   *
-   * @param $key
-   */
-	public function __isset($key) {
-		return array_key_exists($key, $this->_values);
-	}
+              if ($elements->length > 0) {
+                  $domattr = $elements->item(0)->attributes->getNamedItem('href');
+                  if ($domattr) {
+                      $page->_values['image'] = $domattr->value;
+                      $page->_values['image_src'] = $domattr->value;
+                  }
+              } else if (!empty($page->_values['twitter_image'])){
+  				$page->_values['image'] = $page->_values['twitter_image'];
+  			} else {
+  				$elements = $doc->getElementsByTagName("img");
+  				foreach ( $elements as $tag ){
+  					if ($tag->hasAttribute('width') && ($tag->getAttribute('width') > 300) ){
+  						$page->_values['image'] = $tag->getAttribute('src');
+  						break;
+  					}
+  				}
+  			}
+          }
 
-  /**
-   * Will return true if the page has location data embedded
-   *
-   * @return boolean Check if the page has location data
-   */
-	public function hasLocation() {
-		if (array_key_exists('latitude', $this->_values) && array_key_exists('longitude', $this->_values)) {
-			return true;
-		}
+  		if (empty($page->_values)) { return false; }
 
-		$address_keys = array('street_address', 'locality', 'region', 'postal_code', 'country_name');
-		$valid_address = true;
-		foreach ($address_keys AS $key) {
-			$valid_address = ($valid_address && array_key_exists($key, $this->_values));
-		}
-		return $valid_address;
-	}
+  		return $page;
+  	}
 
-  /**
-   * Iterator code
-   */
-	private $_position = 0;
-	public function rewind() { reset($this->_values); $this->_position = 0; }
-	public function current() { return current($this->_values); }
-	public function key() { return key($this->_values); }
-	public function next() { next($this->_values); ++$this->_position; }
-	public function valid() { return $this->_position < sizeof($this->_values); }
+    /**
+     * Helper method to access attributes directly
+     * Example:
+     * $graph->title
+     *
+     * @param $key    Key to fetch from the lookup
+     */
+  	public function __get($key) {
+  		if (array_key_exists($key, $this->_values)) {
+  			return $this->_values[$key];
+  		}
+
+  		if ($key === 'schema') {
+  			foreach (self::$TYPES AS $schema => $types) {
+  				if (array_search($this->_values['type'], $types)) {
+  					return $schema;
+  				}
+  			}
+  		}
+  	}
+
+    /**
+     * Return all the keys found on the page
+     *
+     * @return array
+     */
+  	public function keys() {
+  		return array_keys($this->_values);
+  	}
+
+    /**
+     * Helper method to check an attribute exists
+     *
+     * @param $key
+     */
+  	public function __isset($key) {
+  		return array_key_exists($key, $this->_values);
+  	}
+
+    /**
+     * Will return true if the page has location data embedded
+     *
+     * @return boolean Check if the page has location data
+     */
+  	public function hasLocation() {
+  		if (array_key_exists('latitude', $this->_values) && array_key_exists('longitude', $this->_values)) {
+  			return true;
+  		}
+
+  		$address_keys = array('street_address', 'locality', 'region', 'postal_code', 'country_name');
+  		$valid_address = true;
+  		foreach ($address_keys AS $key) {
+  			$valid_address = ($valid_address && array_key_exists($key, $this->_values));
+  		}
+  		return $valid_address;
+  	}
+
+    /**
+     * Iterator code
+     */
+  	private $_position = 0;
+  	public function rewind() { reset($this->_values); $this->_position = 0; }
+  	public function current() { return current($this->_values); }
+  	public function key() { return key($this->_values); }
+  	public function next() { next($this->_values); ++$this->_position; }
+  	public function valid() { return $this->_position < sizeof($this->_values); }
 }
