f94d49b8298e42fbfd4f0e39ea10c08d112333d9
Aram Zucker-Scharff <aramzs@salon.com>
2016-08-05 00:01:40 -0400

    Fix to assure that pending shows up in quick edit of feeds. Resolves #789

--- assets/js/quick-edit.js
+++ assets/js/quick-edit.js
@@ -18,6 +18,7 @@ window.wp = window.wp || {};
 			setTimeout( function( ) {
 				post_id = inlineEditPost.getId( ev );
 				post_feed_url = $( '#pf-feed-' + post_id + '-url' ).val();
+				var set_post_status = $( '#inline_' + post_id + ' ._status').text();
 				$( '#pf-quick-edit-feed-url' ).val( post_feed_url );
 				jQuery('body.post-type-pf_feed').find('[name="_status"] option').text(
 					function (index, post_status){
@@ -29,6 +30,9 @@ window.wp = window.wp || {};
 					  }
 					}
 				);
+				if ( 'under_review' == set_post_status ){
+					jQuery('body.post-type-pf_feed').find('[name="_status"]').val('pending');
+				}
 				//console.log(pulldownStatus);
 
 
