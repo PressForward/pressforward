08b5d5ffd05758cb69584fae9bd7f92052d9b215
Aram Zucker-Scharff <aramzs@salon.com>
2016-09-05 23:40:58 -0400

    Avoid readability errors when increasing inclusion

--- Libraries/FiveFiltersReadability/Readability.php
+++ Libraries/FiveFiltersReadability/Readability.php
@@ -1117,7 +1117,7 @@ class Readability
 				if ( ( false !== stripos($tagsList->item($i)->textContent, 'et al') ) || ( 1 === preg_match('/\([1-9]\d{3,}\)/', $tagsList->item($i)->textContent) ) ){
 					$this->dbg(' content of element indicates reference.');
 					$toRemove = false;
-				} else if ( ( ( 'li' === $tagsList->item($i)->parentNode->tagName || 'li' === $tagsList->item($i)->parentNode->parentNode->tagName ) && ( false !== stripos($tagsList->item($i-1)->textContent, 'et al') ) ) || ( 1 === preg_match('/\([1-9]\d{3,}\)/', $tagsList->item($i-1)->textContent) ) ){
+				} else if ( ( !empty($tagsList->item($i)->parentNode) ) && ( ( ( 'li' === $tagsList->item($i)->parentNode->tagName || 'li' === $tagsList->item($i)->parentNode->parentNode->tagName ) && ( false !== stripos($tagsList->item($i-1)->textContent, 'et al') ) ) || ( 1 === preg_match('/\([1-9]\d{3,}\)/', $tagsList->item($i-1)->textContent) ) ) ){
 					$this->dbg(' content of element indicates reference.');
 					$toRemove = false;
 				}
