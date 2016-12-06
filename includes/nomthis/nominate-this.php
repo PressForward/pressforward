385cb1a2b083f8a4676c9be8af99deb2a8ee56ae
Aram Zucker-Scharff <aramzs@salon.com>
2016-12-05 21:14:21 -0500

    Simplify Nominate This execution.

--- includes/nomthis/nominate-this.php
+++ includes/nomthis/nominate-this.php
@@ -172,7 +172,7 @@ function nominate_it() {
 	#var_dump($post); die();
 	return $post_ID;
 }
-
+$posted = false;
 // For submitted posts.
 if ( isset($_REQUEST['action']) && 'post' == $_REQUEST['action'] ) {
 	check_admin_referer('nominate-this');
@@ -184,228 +184,228 @@ if ( isset($_REQUEST['action']) && 'post' == $_REQUEST['action'] ) {
 	//$post_ID = $post->ID;
 	//pf_log('Establish post '.$post_ID);
 	//var_dump($_GET['u']); die();
-}
 
-				global $pf_nt;
-				if (isset($_POST['item_link']) && !empty($_POST['item_link']) && ($_POST['item_link']) != ''){
-					pf_log('Getting OpenGraph image on ');
-					pf_log($_POST['item_link']);
-					//var_dump($_POST['item_link']); die();
-					//Gets OG image
-					$itemFeatImg = pressforward('schema.feed_item')->get_ext_og_img($_POST['item_link']);
-					//var_dump($itemFeatImg); die();
-				}
 
-	if ( ( !empty($_REQUEST['action']) && 'post' == $_REQUEST['action'] ) && !empty($_POST['item_link']) && ($_POST['item_link']) != ''){
+	global $pf_nt;
+	if (isset($_POST['item_link']) && !empty($_POST['item_link']) && ($_POST['item_link']) != ''){
+		pf_log('Getting OpenGraph image on ');
+		pf_log($_POST['item_link']);
+		//var_dump($_POST['item_link']); die();
+		//Gets OG image
+		$itemFeatImg = pressforward('schema.feed_item')->get_ext_og_img($_POST['item_link']);
+		//var_dump($itemFeatImg); die();
+	}
+
+	if ( !empty($_POST['item_link']) && ( $_POST['item_link'] != '' ) ){
 		pressforward('schema.feed_item')->set_ext_as_featured($post_ID, $itemFeatImg);
 	}
 
-// Set Variables
+    // Set Variables
 
 
-$selection = '';
-if ( !empty($_GET['s']) ) {
-	$selection = str_replace('&apos;', "'", stripslashes($_GET['s']));
-	$selection = trim( htmlspecialchars( html_entity_decode($selection, ENT_QUOTES) ) );
+    $selection = '';
+    if ( !empty($_GET['s']) ) {
+    	$selection = str_replace('&apos;', "'", stripslashes($_GET['s']));
+    	$selection = trim( htmlspecialchars( html_entity_decode($selection, ENT_QUOTES) ) );
+    }
+
+    if ( ! empty($selection) ) {
+    	$selection = preg_replace('/(\r?\n|\r)/', '</p><p>', $selection);
+    	$selection = '<p>' . str_replace('<p></p>', '', $selection) . '</p>';
+    	$selection = '<blockquote>' . $selection . '</blockquote>';
+    }
+
+    $url = isset($_GET['u']) ? esc_url($_GET['u']) : '';
+    $image = isset($_GET['i']) ? $_GET['i'] : '';
+
+    if ( !empty($_REQUEST['ajax']) ) {
+    	switch ($_REQUEST['ajax']) {
+    		case 'video': ?>
+    			<script type="text/javascript">
+    			/* <![CDATA[ */
+    				jQuery('.select').click(function() {
+    					append_editor(jQuery('#embed-code').val());
+    					jQuery('#extra-fields').hide();
+    					jQuery('#extra-fields').html('');
+    				});
+    				jQuery('.close').click(function() {
+    					jQuery('#extra-fields').hide();
+    					jQuery('#extra-fields').html('');
+    				});
+    			/* ]]> */
+    			</script>
+    			<div class="postbox">
+    				<h2><label for="embed-code"><?php _e('Embed Code', 'pf') ?></label></h2>
+    				<div class="inside">
+    					<textarea name="embed-code" id="embed-code" rows="8" cols="40"><?php echo esc_textarea( $selection ); ?></textarea>
+    					<p id="options"><a href="#" class="select button"><?php _e('Insert Video', 'pf'); ?></a> <a href="#" class="close button"><?php _e('Cancel', 'pf'); ?></a></p>
+    				</div>
+    			</div>
+    			<?php break;
+
+    		case 'photo_thickbox': ?>
+    			<script type="text/javascript">
+    				/* <![CDATA[ */
+    				jQuery('.cancel').click(function() {
+    					tb_remove();
+    				});
+    				jQuery('.select').click(function() {
+    					image_selector(this);
+    				});
+    				/* ]]> */
+    			</script>
+    			<h3 class="tb"><label for="tb_this_photo_description"><?php _e('Description', 'pf'); ?></label></h3>
+    			<div class="titlediv">
+    				<div class="titlewrap">
+    					<input id="tb_this_photo_description" name="photo_description" class="tb_this_photo_description tbtitle text" onkeypress="if(event.keyCode==13) image_selector(this);" value="<?php echo esc_attr($title);?>"/>
+    				</div>
+    			</div>
+
+    			<p class="centered">
+    				<input type="hidden" name="this_photo" value="<?php echo esc_attr($image); ?>" id="tb_this_photo" class="tb_this_photo" />
+    				<a href="#" class="select">
+    					<img src="<?php echo esc_url($image); ?>" alt="<?php echo esc_attr(__('Click to insert.', 'pf')); ?>" title="<?php echo esc_attr(__('Click to insert.', 'pf')); ?>" />
+    				</a>
+    			</p>
+
+    			<p id="options"><a href="#" class="select button"><?php _e('Insert Image','pf'); ?></a> <a href="#" class="cancel button"><?php _e('Cancel','pf'); ?></a></p>
+    			<?php break;
+    	case 'photo_images':
+    		/**
+    		 * Retrieve all image URLs from given URI.
+    		 *
+    		 * @package WordPress
+    		 * @subpackage Press_This
+    		 * @since 2.6.0
+    		 *
+    		 * @param string $uri
+    		 * @return string
+    		 */
+    		function get_images_from_uri($uri) {
+    			$uri = preg_replace('/\/#.+?$/','', $uri);
+    			if ( preg_match( '/\.(jpe?g|jpe|gif|png)\b/i', $uri ) && !strpos( $uri, 'blogger.com' ) )
+    				return "'" . esc_attr( html_entity_decode($uri) ) . "'";
+    			$content = wp_remote_fopen($uri);
+    			if ( false === $content )
+    				return '';
+    			$host = parse_url($uri);
+    			$pattern = '/<img ([^>]*)src=(\"|\')([^<>\'\"]+)(\2)([^>]*)\/*>/i';
+    			$content = str_replace(array("\n","\t","\r"), '', $content);
+    			preg_match_all($pattern, $content, $matches);
+    			if ( empty($matches[0]) )
+    				return '';
+    			$sources = array();
+    			foreach ($matches[3] as $src) {
+    				// if no http in url
+    				if (strpos($src, 'http') === false)
+    					// if it doesn't have a relative uri
+    					if ( strpos($src, '../') === false && strpos($src, './') === false && strpos($src, '/') === 0)
+    						$src = 'http://'.str_replace('//','/', $host['host'].'/'.$src);
+    					else
+    						$src = 'http://'.str_replace('//','/', $host['host'].'/'.dirname($host['path']).'/'.$src);
+    				$sources[] = esc_url($src);
+    			}
+    			return "'" . implode("','", $sources) . "'";
+    		}
+    		$url = wp_kses(urldecode($url), null);
+    		echo 'new Array('.get_images_from_uri($url).')';
+    		break;
+
+    	case 'photo_js': ?>
+    		// gather images and load some default JS
+    		var last = null
+    		var img, img_tag, aspect, w, h, skip, i, strtoappend = "";
+    		if(photostorage == false) {
+    		var my_src = eval(
+    			jQuery.ajax({
+    				type: "GET",
+    				url: "<?php echo esc_url($_SERVER['PHP_SELF']); ?>",
+    				cache : false,
+    				async : false,
+    				data: "ajax=photo_images&u=<?php echo urlencode($url); ?>",
+    				dataType : "script"
+    			}).responseText
+    		);
+    		if(my_src.length == 0) {
+    			var my_src = eval(
+    				jQuery.ajax({
+    					type: "GET",
+    					url: "<?php echo esc_url($_SERVER['PHP_SELF']); ?>",
+    					cache : false,
+    					async : false,
+    					data: "ajax=photo_images&u=<?php echo urlencode($url); ?>",
+    					dataType : "script"
+    				}).responseText
+    			);
+    			if(my_src.length == 0) {
+    				strtoappend = '<?php _e('Unable to retrieve images or no images on page.','pf'); ?>';
+    			}
+    		}
+    		}
+    		for (i = 0; i < my_src.length; i++) {
+    			img = new Image();
+    			img.src = my_src[i];
+    			img_attr = 'id="img' + i + '"';
+    			skip = false;
+
+    			maybeappend = '<a href="?ajax=photo_thickbox&amp;i=' + encodeURIComponent(img.src) + '&amp;u=<?php echo urlencode($url); ?>&amp;height=400&amp;width=500" title="" class="thickbox"><img src="' + img.src + '" ' + img_attr + '/></a>';
+
+    			if (img.width && img.height) {
+    				if (img.width >= 30 && img.height >= 30) {
+    					aspect = img.width / img.height;
+    					scale = (aspect > 1) ? (71 / img.width) : (71 / img.height);
+
+    					w = img.width;
+    					h = img.height;
+
+    					if (scale < 1) {
+    						w = parseInt(img.width * scale);
+    						h = parseInt(img.height * scale);
+    					}
+    					img_attr += ' style="width: ' + w + 'px; height: ' + h + 'px;"';
+    					strtoappend += maybeappend;
+    				}
+    			} else {
+    				strtoappend += maybeappend;
+    			}
+    		}
+
+    		function pick(img, desc) {
+    			if (img) {
+    				if('object' == typeof jQuery('.photolist input') && jQuery('.photolist input').length != 0) length = jQuery('.photolist input').length;
+    				if(length == 0) length = 1;
+    				jQuery('.photolist').append('<input name="photo_src[' + length + ']" value="' + img +'" type="hidden"/>');
+    				jQuery('.photolist').append('<input name="photo_description[' + length + ']" value="' + desc +'" type="hidden"/>');
+    				insert_editor( "\n\n" + encodeURI('<p style="text-align: center;"><a href="<?php echo $url; ?>"><img src="' + img +'" alt="' + desc + '" /></a></p>'));
+    			}
+    			return false;
+    		}
+
+    		function image_selector(el) {
+    			var desc, src, parent = jQuery(el).closest('#photo-add-url-div');
+
+    			if ( parent.length ) {
+    				desc = parent.find('input.tb_this_photo_description').val() || '';
+    				src = parent.find('input.tb_this_photo').val() || ''
+    			} else {
+    				desc = jQuery('#tb_this_photo_description').val() || '';
+    				src = jQuery('#tb_this_photo').val() || ''
+    			}
+
+    			tb_remove();
+    			pick(src, desc);
+    			jQuery('#extra-fields').hide();
+    			jQuery('#extra-fields').html('');
+    			return false;
+    		}
+
+    		jQuery('#extra-fields').html('<div class="postbox"><h2><?php _e( 'Add Photos','pf' ); ?> <small id="photo_directions">(<?php _e("click images to select") ?>)</small></h2><ul class="actions"><li><a href="#" id="photo-add-url" class="button button-small"><?php _e("Add from URL",'pf') ?> +</a></li></ul><div class="inside"><div class="titlewrap"><div id="img_container"></div></div><p id="options"><a href="#" class="close button"><?php _e('Cancel','pf'); ?></a><a href="#" class="refresh button"><?php _e('Refresh','pf'); ?></a></p></div>');
+    		jQuery('#img_container').html(strtoappend);
+    		<?php break;
+    }
+    die;
+    }
 }
-
-if ( ! empty($selection) ) {
-	$selection = preg_replace('/(\r?\n|\r)/', '</p><p>', $selection);
-	$selection = '<p>' . str_replace('<p></p>', '', $selection) . '</p>';
-	$selection = '<blockquote>' . $selection . '</blockquote>';
-}
-
-$url = isset($_GET['u']) ? esc_url($_GET['u']) : '';
-$image = isset($_GET['i']) ? $_GET['i'] : '';
-
-if ( !empty($_REQUEST['ajax']) ) {
-	switch ($_REQUEST['ajax']) {
-		case 'video': ?>
-			<script type="text/javascript">
-			/* <![CDATA[ */
-				jQuery('.select').click(function() {
-					append_editor(jQuery('#embed-code').val());
-					jQuery('#extra-fields').hide();
-					jQuery('#extra-fields').html('');
-				});
-				jQuery('.close').click(function() {
-					jQuery('#extra-fields').hide();
-					jQuery('#extra-fields').html('');
-				});
-			/* ]]> */
-			</script>
-			<div class="postbox">
-				<h2><label for="embed-code"><?php _e('Embed Code', 'pf') ?></label></h2>
-				<div class="inside">
-					<textarea name="embed-code" id="embed-code" rows="8" cols="40"><?php echo esc_textarea( $selection ); ?></textarea>
-					<p id="options"><a href="#" class="select button"><?php _e('Insert Video', 'pf'); ?></a> <a href="#" class="close button"><?php _e('Cancel', 'pf'); ?></a></p>
-				</div>
-			</div>
-			<?php break;
-
-		case 'photo_thickbox': ?>
-			<script type="text/javascript">
-				/* <![CDATA[ */
-				jQuery('.cancel').click(function() {
-					tb_remove();
-				});
-				jQuery('.select').click(function() {
-					image_selector(this);
-				});
-				/* ]]> */
-			</script>
-			<h3 class="tb"><label for="tb_this_photo_description"><?php _e('Description', 'pf'); ?></label></h3>
-			<div class="titlediv">
-				<div class="titlewrap">
-					<input id="tb_this_photo_description" name="photo_description" class="tb_this_photo_description tbtitle text" onkeypress="if(event.keyCode==13) image_selector(this);" value="<?php echo esc_attr($title);?>"/>
-				</div>
-			</div>
-
-			<p class="centered">
-				<input type="hidden" name="this_photo" value="<?php echo esc_attr($image); ?>" id="tb_this_photo" class="tb_this_photo" />
-				<a href="#" class="select">
-					<img src="<?php echo esc_url($image); ?>" alt="<?php echo esc_attr(__('Click to insert.', 'pf')); ?>" title="<?php echo esc_attr(__('Click to insert.', 'pf')); ?>" />
-				</a>
-			</p>
-
-			<p id="options"><a href="#" class="select button"><?php _e('Insert Image','pf'); ?></a> <a href="#" class="cancel button"><?php _e('Cancel','pf'); ?></a></p>
-			<?php break;
-	case 'photo_images':
-		/**
-		 * Retrieve all image URLs from given URI.
-		 *
-		 * @package WordPress
-		 * @subpackage Press_This
-		 * @since 2.6.0
-		 *
-		 * @param string $uri
-		 * @return string
-		 */
-		function get_images_from_uri($uri) {
-			$uri = preg_replace('/\/#.+?$/','', $uri);
-			if ( preg_match( '/\.(jpe?g|jpe|gif|png)\b/i', $uri ) && !strpos( $uri, 'blogger.com' ) )
-				return "'" . esc_attr( html_entity_decode($uri) ) . "'";
-			$content = wp_remote_fopen($uri);
-			if ( false === $content )
-				return '';
-			$host = parse_url($uri);
-			$pattern = '/<img ([^>]*)src=(\"|\')([^<>\'\"]+)(\2)([^>]*)\/*>/i';
-			$content = str_replace(array("\n","\t","\r"), '', $content);
-			preg_match_all($pattern, $content, $matches);
-			if ( empty($matches[0]) )
-				return '';
-			$sources = array();
-			foreach ($matches[3] as $src) {
-				// if no http in url
-				if (strpos($src, 'http') === false)
-					// if it doesn't have a relative uri
-					if ( strpos($src, '../') === false && strpos($src, './') === false && strpos($src, '/') === 0)
-						$src = 'http://'.str_replace('//','/', $host['host'].'/'.$src);
-					else
-						$src = 'http://'.str_replace('//','/', $host['host'].'/'.dirname($host['path']).'/'.$src);
-				$sources[] = esc_url($src);
-			}
-			return "'" . implode("','", $sources) . "'";
-		}
-		$url = wp_kses(urldecode($url), null);
-		echo 'new Array('.get_images_from_uri($url).')';
-		break;
-
-	case 'photo_js': ?>
-		// gather images and load some default JS
-		var last = null
-		var img, img_tag, aspect, w, h, skip, i, strtoappend = "";
-		if(photostorage == false) {
-		var my_src = eval(
-			jQuery.ajax({
-				type: "GET",
-				url: "<?php echo esc_url($_SERVER['PHP_SELF']); ?>",
-				cache : false,
-				async : false,
-				data: "ajax=photo_images&u=<?php echo urlencode($url); ?>",
-				dataType : "script"
-			}).responseText
-		);
-		if(my_src.length == 0) {
-			var my_src = eval(
-				jQuery.ajax({
-					type: "GET",
-					url: "<?php echo esc_url($_SERVER['PHP_SELF']); ?>",
-					cache : false,
-					async : false,
-					data: "ajax=photo_images&u=<?php echo urlencode($url); ?>",
-					dataType : "script"
-				}).responseText
-			);
-			if(my_src.length == 0) {
-				strtoappend = '<?php _e('Unable to retrieve images or no images on page.','pf'); ?>';
-			}
-		}
-		}
-		for (i = 0; i < my_src.length; i++) {
-			img = new Image();
-			img.src = my_src[i];
-			img_attr = 'id="img' + i + '"';
-			skip = false;
-
-			maybeappend = '<a href="?ajax=photo_thickbox&amp;i=' + encodeURIComponent(img.src) + '&amp;u=<?php echo urlencode($url); ?>&amp;height=400&amp;width=500" title="" class="thickbox"><img src="' + img.src + '" ' + img_attr + '/></a>';
-
-			if (img.width && img.height) {
-				if (img.width >= 30 && img.height >= 30) {
-					aspect = img.width / img.height;
-					scale = (aspect > 1) ? (71 / img.width) : (71 / img.height);
-
-					w = img.width;
-					h = img.height;
-
-					if (scale < 1) {
-						w = parseInt(img.width * scale);
-						h = parseInt(img.height * scale);
-					}
-					img_attr += ' style="width: ' + w + 'px; height: ' + h + 'px;"';
-					strtoappend += maybeappend;
-				}
-			} else {
-				strtoappend += maybeappend;
-			}
-		}
-
-		function pick(img, desc) {
-			if (img) {
-				if('object' == typeof jQuery('.photolist input') && jQuery('.photolist input').length != 0) length = jQuery('.photolist input').length;
-				if(length == 0) length = 1;
-				jQuery('.photolist').append('<input name="photo_src[' + length + ']" value="' + img +'" type="hidden"/>');
-				jQuery('.photolist').append('<input name="photo_description[' + length + ']" value="' + desc +'" type="hidden"/>');
-				insert_editor( "\n\n" + encodeURI('<p style="text-align: center;"><a href="<?php echo $url; ?>"><img src="' + img +'" alt="' + desc + '" /></a></p>'));
-			}
-			return false;
-		}
-
-		function image_selector(el) {
-			var desc, src, parent = jQuery(el).closest('#photo-add-url-div');
-
-			if ( parent.length ) {
-				desc = parent.find('input.tb_this_photo_description').val() || '';
-				src = parent.find('input.tb_this_photo').val() || ''
-			} else {
-				desc = jQuery('#tb_this_photo_description').val() || '';
-				src = jQuery('#tb_this_photo').val() || ''
-			}
-
-			tb_remove();
-			pick(src, desc);
-			jQuery('#extra-fields').hide();
-			jQuery('#extra-fields').html('');
-			return false;
-		}
-
-		jQuery('#extra-fields').html('<div class="postbox"><h2><?php _e( 'Add Photos','pf' ); ?> <small id="photo_directions">(<?php _e("click images to select") ?>)</small></h2><ul class="actions"><li><a href="#" id="photo-add-url" class="button button-small"><?php _e("Add from URL",'pf') ?> +</a></li></ul><div class="inside"><div class="titlewrap"><div id="img_container"></div></div><p id="options"><a href="#" class="close button"><?php _e('Cancel','pf'); ?></a><a href="#" class="refresh button"><?php _e('Refresh','pf'); ?></a></p></div>');
-		jQuery('#img_container').html(strtoappend);
-		<?php break;
-}
-die;
-}
-
 	wp_enqueue_style( 'colors' );
 	wp_enqueue_script( 'post' );
 	_wp_admin_html_begin();
@@ -611,6 +611,7 @@ $admin_body_class .= ' locale-' . sanitize_html_class( strtolower( str_replace( 
 				} else {
 					$tags_retrieved = 'via bookmarklet';
 				}
+                //var_dump($og); die();
 				//$response_body = wp_remote_retrieve_body( $response );
 				//$response_dom = pf_str_get_html( $response_body );
 				if ( isset($og->url) ){
@@ -749,7 +750,9 @@ $admin_body_class .= ' locale-' . sanitize_html_class( strtolower( str_replace( 
 			<?php do_action('nominate_this_sidebar_bottom'); ?>
 		</div>
 	</div>
-    <?php } ?>
+    <?php }
+// Post complete template
+?>
 	<div class="posting">
 
 		<div id="wphead">
