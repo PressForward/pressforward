a32d5be679c0fcf9974e4c9e83e1878467bf28cb
Aram Zucker-Scharff <aramzs@salon.com>
2017-04-10 11:58:03 -0400

    Testing nomination authentication

--- Core/API/NominateThisEndpoint.php
+++ Core/API/NominateThisEndpoint.php
@@ -18,7 +18,9 @@ class NominateThisEndpoint implements HasActions {
 		$this->templates = $templates;
 		$namespace = $this->api_base['base_namespace'] . $this->api_base['version'];
 		$base = $this->api_base['endpoint'];
+		$this->api_base['authpoint'] = 'nominate';
 		$this->endpoint_for_nominate_this_endpoint = $namespace. '/' . $base;
+		$this->endpoint_for_nominate_endpoint = $namespace. '/' . 	$this->api_base['authpoint'];
 	}
 
 	public function action_hooks() {
@@ -60,6 +62,10 @@ class NominateThisEndpoint implements HasActions {
 			      ),
 			    ),
 				'permission_callback' => function () {
+					//return true;
+					if (true === rest_cookie_check_errors()){
+						return true;
+					}
 					$nominate_access = get_option('pf_menu_all_content_access');
   				  //pressforward('admin.templates')->users->pf_get_defining_capability_by_role( 'contributor' );//get_option('pf_menu_all_content_access');
 	  				if ( ! current_user_can( get_option( 'pf_menu_nominate_this_access', pressforward( 'controller.users' )->pf_get_defining_capability_by_role( 'contributor' ) ) ) ){
@@ -72,6 +78,54 @@ class NominateThisEndpoint implements HasActions {
 				'priority'  => 10,
 			),
 		));
+		register_rest_route($namespace, '/'.$this->api_base['authpoint'], array(
+			array(
+				'methods'         => \WP_REST_Server::READABLE,
+				'callback'        => array( $this, 'get_nominate_this' ),
+				'args' => array(
+				  'context' => array(
+					  // description should be a human readable description of the argument.
+					'description' => esc_html__( 'Supplies the Nominate This template for building the bookmarklet.', 'pf' ),
+					// type specifies the type of data that the argument should be.
+					'type'        => 'string',
+					// Set the argument to be required for the endpoint.
+					'required'    => false,
+					'default'	  => 'view'
+				  ),
+				  'u' => array(
+					  // description should be a human readable description of the argument.
+					'description' => esc_html__( 'Supplies the Nominate This template for building the bookmarklet.', 'pf' ),
+					// type specifies the type of data that the argument should be.
+					'type'        => 'string',
+					// Set the argument to be required for the endpoint.
+					'required'    => false,
+					'default'	  => 'view'
+				  ),
+				  's' => array(
+					  // description should be a human readable description of the argument.
+					'description' => esc_html__( 'Supplies the Nominate This template for building the bookmarklet.', 'pf' ),
+					// type specifies the type of data that the argument should be.
+					'type'        => 'string',
+					// Set the argument to be required for the endpoint.
+					'required'    => false,
+					'default'	  => 'view'
+				  ),
+				  'v' => array(
+					  // description should be a human readable description of the argument.
+					'description' => esc_html__( 'Supplies the Nominate This template for building the bookmarklet.', 'pf' ),
+					// type specifies the type of data that the argument should be.
+					'type'        => 'string',
+					// Set the argument to be required for the endpoint.
+					'required'    => false,
+					'default'	  => ''
+				  ),
+				),
+				'permission_callback' => function () {
+					return true;
+				},
+				'priority'  => 10,
+			),
+		));
 
 	}
 
@@ -79,9 +133,27 @@ class NominateThisEndpoint implements HasActions {
 		define( 'IFRAME_REQUEST' , true );
 		define( 'WP_ADMIN', false );
 		global $pagenow;
+		//wp_verify_nonce( $nonce, 'wp_rest' );
 		return rest_ensure_response(
 				include( PF_ROOT.'/includes/nomthis/nominate-this-core.php' )
 			);
+	}
+
+	public function get_nominate_this($request){
+		define( 'IFRAME_REQUEST' , true );
+		define( 'WP_ADMIN', true );
+		//var_dump($request); var_dump($_GET); die();
+		global $pagenow;
+		header( 'Content-Type: ' . get_option( 'html_type' ) . '; charset=' . get_option( 'blog_charset' ) );
+		$nonce = wp_create_nonce( 'wp_rest' );
+		ob_start();
+		wp_login_form(array(
+			'redirect'	=>	rest_url($this->endpoint_for_nominate_this_endpoint.'?nonce='.$nonce, 'html')
+		));
+		$login = ob_get_clean();
+		echo $login;
+		die();
+
 	}
 
 
