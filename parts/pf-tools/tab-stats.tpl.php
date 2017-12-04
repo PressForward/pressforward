391f5797f81a9e51d059d440c6c6a2f1e045685a
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2017-12-04 14:57:47 -0500

    Noting that this requires the WP API
    #968

--- parts/pf-tools/tab-stats.tpl.php
+++ parts/pf-tools/tab-stats.tpl.php
@@ -1,6 +1,6 @@
 <div id="stats">
 	<div style="width: 100%">
-		<p>About your PressForward Install</p>
+		<p>About your PressForward Install (Requires the WordPress REST API.)</p>
 		<p id="top-level"></p>
 		<a href="#" id="build-authors" class="button" onclick="window.pf.stats.authors.getLeaderboard(true);">Assemble Author Stats</a>
 		<a href="#" id="build-valid-posts" class="button" onclick="window.pf.stats.valid_posts.getLeaderboard(true);">Assemble Valid Posts for Stats</a>
