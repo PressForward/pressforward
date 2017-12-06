200263d0facae059cc84a92a725dde00c3453a1c
Aram Zucker-Scharff <Aram.Zucker-Scharff@washpost.com>
2017-12-05 20:37:09 -0500

    More mocks from issues

--- tests/test-nomination-process.php
+++ tests/test-nomination-process.php
@@ -504,17 +504,41 @@ class PF_Tests_Nomination_Process extends PF_UnitTestCase {
 		$user_id = $this->factory->user->create( array( 'role' => 'administrator', 'user_login' => 'bookmarklet_to_nomination' ) );
 		wp_set_current_user( $user_id );
 		$_POST = array();
-		$title = "bookmarklet test";
+		$title = "Trump should skip civil-rights museum opening, NAACP says, calling his plans to attend ‘an insult’ - The Washington Post";
 		$time = time();
 		$post = array(
 			'post_title' => $title,
-			'item_link' => 'http://aramzs.github.io/notes/wordpress/wordpressus2015/2015/12/04/wordcamp-us.html?t=9',
-			'item_content' => 'Test content',
-			'source_title' => 'Test source title',
+			'item_link' => 'https://www.washingtonpost.com/news/politics/wp/2017/12/05/naacp-says-that-trump-should-not-attend-opening-of-civil-rights-museum-in-mississippi/',
+			'item_content' => 'Ellie Dahmer, wife of Vernon Dahmer, who was killed in 1966 by the Ku Klux Klan, views some of the artifacts in the Mississippi Civil Rights Museum during a private preview in Jackson. The galleries at the museum focus on the period between 1945 and 1976 when the civil rights movement gained footing in the state. (Rogelio V. Solis/AP)
+
+The NAACP is urging President Trump to skip the opening celebration for a civil rights museum in Mississippi that he had planned to attend, with the organization’s leader sharply criticizing the president’s record on civil rights.
+
+The Mississippi Civil Rights Museum, a project of the state’s Department of Archives and History, is set to open with a ribbon-cutting ceremony on Saturday in Jackson, the state capital. The event will feature speeches from civil rights leaders and elected officials, including Mississippi Gov. Phil Bryant (R), who extended the invitation to the president.
+
+But NAACP President Derrick Johnson said Trump’s attendance would be an “affront” to the movement commemorated by the museum.
+
+“President Trump’s statements and policies regarding the protection and enforcement of civil rights have been abysmal, and his attendance is an affront to the veterans of the civil rights movement,” Johnson said. “He has created a commission to reinforce voter suppression, refused to denounce white supremacists, and overall, has created a racially hostile climate in this nation.”
+
+White House press secretary Sarah Huckabee Sanders called Johnson’s stance “honestly very sad,” during her afternoon briefing on Tuesday.
+
+“I think this is something that should bring the country together,” she said. “And I would hope that those individuals would join in that celebration instead of protesting it.”
+
+The NAACP, founded in 1909, is one of the country’s oldest and most prestigious civil rights organizations. The organization’s rebuke is likely to touch off a new round of debate about the president’s views on racial minorities and civil rights.
+
+Some of the president’s actions, including his refusal at times to disavow white supremacists and his vocal support for Confederate monuments, have been the subject of fierce debate.
+
+The opening, which will celebrate the Civil Rights Museum as well as a new Museum of Mississippi History, is expected to feature speeches from the governor, Bryant; former NAACP chairman Myrlie Evers; and Rep. John Lewis (D-Ga.), a civil rights leader whom Trump disparaged as “All talk, talk, talk — no action or results” after Lewis said he would not attend the president’s inauguration.
+
+NAACP board member Amos Brown, the president of the organization’s San Francisco branch and a native of Jackson, called Trump’s potential visit “an insult.”
+
+“He has never been a supporter of civil rights or equal opportunity or justice,” Brown said in a statement. “He’s been silent on civil rights issues, and his silence speaks volumes.',
+			'source_title' => 'The Washington Post',
 			'sortable_item_date' => 10000,
 			'item_date' => 20000,
 			'item_author' => 'foo',
 			'item_feat_img' => 'Test feat img',
+			'post_date'	=>	'2017-12-06 00:28:51',
+			'post_date_gmt'	=>	'2017-12-06 00:28:51',
 			'item_wp_date' => $time,
 			'post_tags'	=> 'test'
 		);
@@ -591,17 +615,41 @@ class PF_Tests_Nomination_Process extends PF_UnitTestCase {
 		$user_id = $this->factory->user->create( array( 'role' => 'administrator', 'user_login' => 'bookmarklet_to_nomination' ) );
 		wp_set_current_user( $user_id );
 		$_POST = array();
-		$title = "bookmarklet test";
+		$title = "Trump should skip civil-rights museum opening, NAACP says, calling his plans to attend ‘an insult’ - The Washington Post";
 		$time = time();
 		$post = array(
 			'post_title' => $title,
-			'item_link' => 'http://aramzs.github.io/notes/wordpress/wordpressus2015/2015/12/04/wordcamp-us.html?t=239',
-			'item_content' => 'Test content',
-			'source_title' => 'Test source title',
+			'item_link' => 'https://www.washingtonpost.com/news/politics/wp/2017/12/05/naacp-says-that-trump-should-not-attend-opening-of-civil-rights-museum-in-mississippi/',
+			'item_content' => 'Ellie Dahmer, wife of Vernon Dahmer, who was killed in 1966 by the Ku Klux Klan, views some of the artifacts in the Mississippi Civil Rights Museum during a private preview in Jackson. The galleries at the museum focus on the period between 1945 and 1976 when the civil rights movement gained footing in the state. (Rogelio V. Solis/AP)
+
+The NAACP is urging President Trump to skip the opening celebration for a civil rights museum in Mississippi that he had planned to attend, with the organization’s leader sharply criticizing the president’s record on civil rights.
+
+The Mississippi Civil Rights Museum, a project of the state’s Department of Archives and History, is set to open with a ribbon-cutting ceremony on Saturday in Jackson, the state capital. The event will feature speeches from civil rights leaders and elected officials, including Mississippi Gov. Phil Bryant (R), who extended the invitation to the president.
+
+But NAACP President Derrick Johnson said Trump’s attendance would be an “affront” to the movement commemorated by the museum.
+
+“President Trump’s statements and policies regarding the protection and enforcement of civil rights have been abysmal, and his attendance is an affront to the veterans of the civil rights movement,” Johnson said. “He has created a commission to reinforce voter suppression, refused to denounce white supremacists, and overall, has created a racially hostile climate in this nation.”
+
+White House press secretary Sarah Huckabee Sanders called Johnson’s stance “honestly very sad,” during her afternoon briefing on Tuesday.
+
+“I think this is something that should bring the country together,” she said. “And I would hope that those individuals would join in that celebration instead of protesting it.”
+
+The NAACP, founded in 1909, is one of the country’s oldest and most prestigious civil rights organizations. The organization’s rebuke is likely to touch off a new round of debate about the president’s views on racial minorities and civil rights.
+
+Some of the president’s actions, including his refusal at times to disavow white supremacists and his vocal support for Confederate monuments, have been the subject of fierce debate.
+
+The opening, which will celebrate the Civil Rights Museum as well as a new Museum of Mississippi History, is expected to feature speeches from the governor, Bryant; former NAACP chairman Myrlie Evers; and Rep. John Lewis (D-Ga.), a civil rights leader whom Trump disparaged as “All talk, talk, talk — no action or results” after Lewis said he would not attend the president’s inauguration.
+
+NAACP board member Amos Brown, the president of the organization’s San Francisco branch and a native of Jackson, called Trump’s potential visit “an insult.”
+
+“He has never been a supporter of civil rights or equal opportunity or justice,” Brown said in a statement. “He’s been silent on civil rights issues, and his silence speaks volumes.',
+			'source_title' => 'The Washington Post',
 			'sortable_item_date' => 10000,
 			'item_date' => 20000,
 			'item_author' => 'foo',
 			'item_feat_img' => 'Test feat img',
+			'post_date'	=>	'2017-12-06 00:28:51',
+			'post_date_gmt'	=>	'2017-12-06 00:28:51',
 			'item_wp_date' => $time,
 			'post_tags'	=> 'test'
 		);
