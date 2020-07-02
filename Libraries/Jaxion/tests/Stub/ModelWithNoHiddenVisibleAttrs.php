027c5d3bdf3e8d87551e7efbf14f6c4d832b3906
Boone B Gorges <boonebgorges@gmail.com>
2020-07-02 07:22:37 -0500

    Remove library test files.

--- Libraries/Jaxion/tests/Stub/ModelWithNoHiddenVisibleAttrs.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Intraxia\Jaxion\Test\Stub;
-
-class ModelWithNoHiddenVisibleAttrs extends PostAndMetaModel {
-	protected $hidden = array();
-
-	protected $visible = array();
-}
--- Libraries/Jaxion/tests/Stubs/MetaBase.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Intraxia\Jaxion\Test\Stubs;
-
-use Intraxia\Jaxion\Model\Base;
-
-class MetaBase extends Base {
-
-}
