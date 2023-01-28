101bc16dc94a4600590fcc8b8b234a36cde5a22f
Boone B Gorges <boonebgorges@gmail.com>
2023-01-28 07:45:59 -0600

    PHPCS for Controllers/PF_to_WP_Posts.php.
    See #1116.

--- Controllers/PF_to_WP_Posts.php
+++ Controllers/PF_to_WP_Posts.php
@@ -1,17 +1,32 @@
 <?php
+/**
+ * Post utilities.
+ *
+ * @package PressForward
+ */
 
 namespace PressForward\Controllers;
 
 use PressForward\Interfaces\Items as Items;
 
-// use WP_Query;
+/**
+ * Wrappers for WP post functions.
+ */
 class PF_to_WP_Posts implements Items {
 
+	/**
+	 * Creates a post.
+	 *
+	 * @param array $post    Update params. See wp_update_post().
+	 * @param bool  $error   Whether to return a WP_Error object. Default fals.
+	 * @param int   $item_id PF item id.
+	 * @return mixed
+	 */
 	public function insert_post( $post, $error = false, $item_id = false ) {
 		if ( isset( $post['post_date'] ) && ! isset( $post['post_date_gmt'] ) ) {
 			$post['post_date_gmt'] = get_gmt_from_date( $post['post_date'] );
 		}
-		if ( ( false !== $item_id ) && in_array( $post['post_type'], array( 'post', 'pf_feed_item', 'nomination' ) ) ) {
+		if ( ( false !== $item_id ) && in_array( $post['post_type'], array( 'post', 'pf_feed_item', 'nomination' ), true ) ) {
 			$check = $this->check_not_existing( $item_id, $post['post_type'] );
 			if ( true !== $check ) {
 				return $check;
@@ -20,6 +35,13 @@ class PF_to_WP_Posts implements Items {
 		return wp_insert_post( $post, $error );
 	}
 
+	/**
+	 * Updates a post.
+	 *
+	 * @param array $post  Update params. See wp_update_post().
+	 * @param bool  $error Whether to return a WP_Error object. Default fals.
+	 * @return mixed
+	 */
 	public function update_post( $post, $error = false ) {
 		if ( ! is_object( $post ) && ( isset( $post['post_date'] ) && ! isset( $post['post_date_gmt'] ) ) ) {
 			$post['post_date_gmt'] = get_gmt_from_date( $post['post_date'] );
@@ -29,25 +51,62 @@ class PF_to_WP_Posts implements Items {
 		return wp_update_post( $post, $error );
 	}
 
+	/**
+	 * Deletes a post.
+	 *
+	 * @param int|WP_Post $postid       Post.
+	 * @param bool        $force_delete Whether to force delete and skip trash.
+	 * @return mixed
+	 */
 	public function delete_post( $postid, $force_delete = false ) {
 		return wp_delete_post( $postid, $force_delete );
 	}
 
+	/**
+	 * Gets a post object.
+	 *
+	 * @param int|WP_Post $post   Post.
+	 * @param mixed       $output Output format.
+	 * @param string      $filter Filter.
+	 * @return mixed
+	 */
 	public function get_post( $post = null, $output = OBJECT, $filter = 'raw' ) {
 		return get_post( $post, $output, $filter );
 	}
 
+	/**
+	 * Gets posts based on query vars.
+	 *
+	 * @param array $query See get_posts().
+	 * @return array
+	 */
 	public function get_posts( $query ) {
 		return get_posts( $query );
 	}
 
+	/**
+	 * Checks whether a return value is an error.
+	 *
+	 * @param mixed $post Post object returned from wp_update_post().
+	 * @return bool
+	 */
 	public function is_error( $post ) {
 		return is_wp_error( $post );
 	}
 
+	/**
+	 * Checks to see whether an item exists with this PF item ID.
+	 *
+	 * @param int $item_id   PF item ID.
+	 * @param int $post_type Post type.
+	 * @return int
+	 */
 	public function check_not_existing( $item_id, $post_type ) {
 		global $wpdb;
 		$item_id_key = pressforward( 'controller.metas' )->get_key( 'item_id' );
+
+		// Ignoring until we remove.
+		// phpcs:disable
 		if ( 3.2 >= get_bloginfo( 'version' ) ) {
 			$querystr = $wpdb->prepare(
 				"
@@ -74,6 +133,8 @@ class PF_to_WP_Posts implements Items {
 				return $checkposts[0]->ID;
 			}
 		}
+		// phpcs:enable
+
 		pf_log( 'Checking with post 3.2 method.' );
 
 		// WP_Query arguments.
