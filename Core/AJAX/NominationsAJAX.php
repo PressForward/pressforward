e7943753b416de409d64bd31047ecd8ab97f7607
Boone B Gorges <boonebgorges@gmail.com>
2023-01-30 20:03:09 -0600

    PHPCS for Core/AJAX/NominationsAJAX.php.
    See #1116.

--- Core/AJAX/NominationsAJAX.php
+++ Core/AJAX/NominationsAJAX.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * AJAX handlers for Nominations actions.
+ *
+ * @package PressForward
+ */
+
 namespace PressForward\Core\AJAX;
 
 use Intraxia\Jaxion\Contract\Core\HasActions;
@@ -6,21 +12,58 @@ use PressForward\Controllers\Metas;
 use PressForward\Controllers\PF_to_WP_Posts;
 use PressForward\Core\Schema\Feed_Items;
 
+/**
+ * AJAX handlers for Nominations actions.
+ */
 class NominationsAJAX implements HasActions {
-
+	/**
+	 * Basename.
+	 *
+	 * @access protected
+	 * @var string
+	 */
 	protected $basename;
+
+	/**
+	 * Metas object.
+	 *
+	 * @access public
+	 * @var PressForward\Controllers\Metas
+	 */
 	public $metas;
+
+	/**
+	 * PF_to_WP_Posts object.
+	 *
+	 * @access public
+	 * @var PressForward\Controllers\PF_to_WP_Posts
+	 */
 	public $posts;
+
+	/**
+	 * Feed_Items object.
+	 *
+	 * @access public
+	 * @var PressForward\Core\Schema\Feed_Items
+	 */
 	public $items;
 
-	function __construct( Metas $metas, PF_to_WP_Posts $posts, Feed_Items $items ) {
+	/**
+	 * Constructor.
+	 *
+	 * @param PressForward\Controllers\Metas          $metas Metas object.
+	 * @param PressForward\Controllers\PF_to_WP_Posts $posts PF_to_WP_Posts object.
+	 * @param PressForward\Core\Schema\Feed_Items     $items Feed_Items object.
+	 */
+	public function __construct( Metas $metas, PF_to_WP_Posts $posts, Feed_Items $items ) {
 		$this->metas = $metas;
 		$this->posts = $posts;
 		$this->items = $items;
-
 	}
 
-
+	/**
+	 * {@inheritdoc}
+	 */
 	public function action_hooks() {
 		return array(
 			array(
@@ -42,21 +85,33 @@ class NominationsAJAX implements HasActions {
 		);
 	}
 
+	/**
+	 * AJAX handler for 'wp_ajax_build_a_nomination'.
+	 */
 	public function build_a_nomination() {
 		pressforward( 'admin.nominated' )->build_nomination();
 		die();
 	}
 
+	/**
+	 * AJAX handler for 'wp_ajax_build_a_nom_draft'.
+	 */
 	public function build_a_nom_draft() {
 		pressforward( 'admin.nominated' )->build_nom_draft();
 		die();
 	}
 
+	/**
+	 * AJAX handler for 'wp_ajax_archive_a_nom'.
+	 */
 	public function archive_a_nom() {
 		pressforward( 'admin.nominated' )->archive_a_nom();
 		die();
 	}
 
+	/**
+	 * AJAX handler for 'wp_ajax_simple_nom_to_draft'.
+	 */
 	public function simple_nom_to_draft() {
 		pressforward( 'admin.nominated' )->simple_nom_to_draft();
 		die();
