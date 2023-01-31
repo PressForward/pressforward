8545ba95e939a438e7204e1d2bcc1dd2eddc661e
Boone B Gorges <boonebgorges@gmail.com>
2023-01-31 14:06:26 -0600

    PHPCS for Core/Models/Nomination.php.
    See #1116.

--- Core/Models/Nomination.php
+++ Core/Models/Nomination.php
@@ -1,8 +1,24 @@
 <?php
+/**
+ * Feed item model.
+ *
+ * @package PressForward
+ */
+
 namespace PressForward\Core\Models;
 
+/**
+ * Feed_Item class.
+ */
 class Nomination extends BasicModel {
-	function init( $item = array(), $handlers = array(), $post_type = false ) {
+	/**
+	 * Initalizes model.
+	 *
+	 * @param array $item      Data about item.
+	 * @param array $handlers  Controllers.
+	 * @param array $post_type Post type name.
+	 */
+	public function init( $item = array(), $handlers = array(), $post_type = false ) {
 		$this->type     = 'nomination';
 		$this->type_key = 'nomination';
 	}
