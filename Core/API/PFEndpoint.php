a797cde138aae50159f887b3e39ed52b4a42147f
Boone B Gorges <boonebgorges@gmail.com>
2023-01-31 07:11:26 -0600

    PHPCS for Core/API/PFEndpoint.php.
    See #1116.

--- Core/API/PFEndpoint.php
+++ Core/API/PFEndpoint.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Loader for many of PF's endpoints.
+ *
+ * @package PressForward
+ */
+
 namespace PressForward\Core\API;
 
 use Intraxia\Jaxion\Contract\Core\HasActions;
@@ -9,19 +15,57 @@ use PressForward\Core\API\APIWithMetaEndpoints;
 
 use WP_Ajax_Response;
 
+/**
+ * Loader for many of PF's endpoints.
+ */
 class PFEndpoint extends APIWithMetaEndpoints implements HasActions {
 
+	/**
+	 * Basename.
+	 *
+	 * @access protected
+	 * @var string
+	 */
 	protected $basename;
+
+	/**
+	 * Metas object.
+	 *
+	 * @access public
+	 * @var PressForward\Controllers\Metas
+	 */
 	public $metas;
+
+	/**
+	 * Post type.
+	 *
+	 * @access public
+	 * @var string
+	 */
 	public $post_type;
+
+	/**
+	 * Level.
+	 *
+	 * @access public
+	 * @var string
+	 */
 	public $level;
 
-	function __construct( Metas $metas ) {
+	/**
+	 * Constructor.
+	 *
+	 * @param PressForward\Controllers\Metas $metas Metas object.
+	 */
+	public function __construct( Metas $metas ) {
 		$this->metas     = $metas;
 		$this->post_type = pressforward( 'schema.feed_item' )->post_type;
 		$this->level     = 'item';
 	}
 
+	/**
+	 * {@inheritdoc}
+	 */
 	public function action_hooks() {
 		$actions = array(
 			array(
@@ -32,6 +76,9 @@ class PFEndpoint extends APIWithMetaEndpoints implements HasActions {
 		return $actions;
 	}
 
+	/**
+	 * Loads PF endpoint classes.
+	 */
 	public function callback_init() {
 		require_once 'PF_REST_Controller.php';
 		require_once 'PF_Stats_Controller.php';
@@ -39,13 +86,18 @@ class PFEndpoint extends APIWithMetaEndpoints implements HasActions {
 		require_once 'PF_REST_Post_Types_Controller.php';
 		require_once 'PF_REST_Posts_Controller.php';
 		require_once 'PF_REST_Terms_Controller.php';
-
 	}
 
-	function allow_meta_query( $valid_vars ) {
-
+	/**
+	 * Adds 'meta_key' and 'meta_value' to array of allowed args.
+	 *
+	 * @todo Unused.
+	 *
+	 * @param array $valid_vars Array of valid vars.
+	 * @return array
+	 */
+	public function allow_meta_query( $valid_vars ) {
 		$valid_vars = array_merge( $valid_vars, array( 'meta_key', 'meta_value' ) );
 		return $valid_vars;
 	}
-
 }
