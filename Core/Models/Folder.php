c612db3e96780b090bb3cdf103e83ad858290a1c
Boone B Gorges <boonebgorges@gmail.com>
2023-01-31 14:05:44 -0600

    PHPCS for Core/Models/Folder.php.
    See #1116.

--- Core/Models/Folder.php
+++ Core/Models/Folder.php
@@ -1,10 +1,32 @@
 <?php
+/**
+ * Folder model.
+ *
+ * @package PressForward
+ */
+
 namespace PressForward\Core\Models;
 
+/**
+ * Feed_Item class.
+ */
 class Folder extends BasicModel {
+	/**
+	 * Data array.
+	 *
+	 * @access protected
+	 * @var array
+	 */
 	protected $data = array();
 
-	function init( $item = array(), $handlers = array(), $post_type = false ) {
+	/**
+	 * Initalizes model.
+	 *
+	 * @param array $item      Data about item.
+	 * @param array $handlers  Controllers.
+	 * @param array $post_type Post type name.
+	 */
+	public function init( $item = array(), $handlers = array(), $post_type = false ) {
 		$this->type     = 'folder';
 		$this->type_key = 'folder';
 	}
