d063c92ab754079fad8f9c049d2edda01ced869e
Boone B Gorges <boonebgorges@gmail.com>
2023-01-31 07:21:19 -0600

    PHPCS for Core/API/PF_REST_Post_Types_Controller.php.
    See #1116.

--- Core/API/PF_REST_Post_Types_Controller.php
+++ Core/API/PF_REST_Post_Types_Controller.php
@@ -1,32 +1,47 @@
 <?php
+/**
+ * PF_REST_Post_Types_Controller class.
+ *
+ * @package PressForward
+ */
 
 /**
  * Extend the main WP_REST_Posts_Controller to a private endpoint controller.
  */
 class PF_REST_Post_Types_Controller extends WP_REST_Post_Types_Controller {
-
+	/**
+	 * Constructor.
+	 */
 	public function __construct() {
 		parent::__construct();
+
 		$this->namespace = 'pf/v1';
-		$this->terms     = array(
+
+		$this->terms = array(
 			'feeds',
 			'feed_items',
 			'nominations',
 		);
-		$this->types     = array(
+
+		$this->types = array(
 			'pf_feed',
 			'pf_feed_item',
 			'nomination',
 		);
+
 		// Why is this post_types instead of using the basename like elsewhere?
 		// Something to PR later mby.
 		add_filter( 'rest_prepare_post_type', array( $this, 'filter_post_types' ), 11, 3 );
 	}
 
-	public function register_routes() {
-		parent::register_routes();
-	}
-
+	/**
+	 * Adds PF links data to post data in API response.
+	 *
+	 * @param WP_REST_Response $data    Response data.
+	 * @param WP_Post          $post    Post data.
+	 * @param WP_REST_Request  $request Request data.
+	 * @return WP_REST_Response
+	 */
 	public function filter_post_types( $data, $post, $request ) {
 		$links    = $data->get_links();
 		$pf_terms = $this->terms;
@@ -37,15 +52,14 @@ class PF_REST_Post_Types_Controller extends WP_REST_Post_Types_Controller {
 	}
 
 	/**
-	 * Get all public post types
+	 * {@inheritdoc}
 	 *
-	 * @param WP_REST_Request $request
-	 * @return array|WP_Error
+	 * @param WP_REST_Request $request Request object.
 	 */
 	public function get_items( $request ) {
 		$data = array();
 		foreach ( get_post_types( array(), 'object' ) as $obj ) {
-			if ( empty( $obj->show_in_rest ) || ( 'edit' === $request['context'] && ! current_user_can( $obj->cap->edit_posts ) ) || ! in_array( $obj->name, $this->types ) ) {
+			if ( empty( $obj->show_in_rest ) || ( 'edit' === $request['context'] && ! current_user_can( $obj->cap->edit_posts ) ) || ! in_array( $obj->name, $this->types, true ) ) {
 				continue;
 			}
 			$post_type          = $this->prepare_item_for_response( $obj, $request );
@@ -54,23 +68,26 @@ class PF_REST_Post_Types_Controller extends WP_REST_Post_Types_Controller {
 		return rest_ensure_response( $data );
 	}
 
+	/**
+	 * {@inheritdoc}
+	 *
+	 * @param WP_REST_Request $request Request object.
+	 */
 	public function get_item( $request ) {
 		$obj = get_post_type_object( $request['type'] );
-		if ( empty( $obj ) || ! in_array( $obj->name, $this->types ) ) {
-			return new WP_Error( 'rest_type_invalid', __( 'Invalid PF resource.' ), array( 'status' => 404 ) );
+		if ( empty( $obj ) || ! in_array( $obj->name, $this->types, true ) ) {
+			return new WP_Error( 'rest_type_invalid', __( 'Invalid PF resource.', 'pf' ), array( 'status' => 404 ) );
 		} else {
 			return parent::get_item( $request );
 		}
 	}
-
 }
 
+/**
+ * Loads endpoint for PF post types.
+ */
 function activate_pf_post_types_controller() {
 	$controller = new PF_REST_Post_Types_Controller();
 	return $controller->register_routes();
 }
-
 add_action( 'rest_api_init', 'activate_pf_post_types_controller', 11 );
-
-// $controller = new PF_REST_Posts_Controller;
-// $controller->register_routes();
