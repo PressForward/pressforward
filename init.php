621af8eb8c644609828dd73a6f50446608f9cd47
Boone B Gorges <boonebgorges@gmail.com>
2023-02-01 14:27:58 -0600

    PHPCS for init.php.
    See #1116.

--- init.php
+++ init.php
@@ -1,44 +1,56 @@
 <?php
-// Out-of-namespace dependencies
+/**
+ * Plugin bootstrap.
+ *
+ * @package Pressforward
+ */
+
+// Out-of-namespace dependencies.
 require 'class-SplClassLoader.php';
 require 'Libraries/PFSimpleHtmlDom.php';
 require_once 'Libraries/random_compat/lib/random.php';
 require_once 'Libraries/php-jwt/src/JWT.php';
 require 'Libraries/URLResolver/URLResolver.php';
-//require 'Libraries/text-stats/src/DaveChild/TextStatistics/Text.php';
-//require 'Libraries/text-stats/src/DaveChild/TextStatistics/TextStatistics.php';
 require 'Libraries/FiveFiltersReadability/Readability.php';
 require 'Libraries/AlertBox/The_Alert_Box.php';
 require 'includes/opml/object.php';
 require 'includes/opml/reader.php';
 require 'includes/opml/maker.php';
 require 'Libraries/PFOpenGraph.php';
-// Files included to maintain back-compat
+
+// Files included to maintain back-compat.
 require 'includes/functions.php';
 require 'includes/relationships.php';
 require 'includes/template-tags.php';
 
-
 use Intraxia\Jaxion\Core\Config;
 use Intraxia\Jaxion\Core\ConfigType;
 use SplClassLoader as ClassLoader;
-$tsClassLoader = new ClassLoader( 'DaveChild', 'Libraries/text-stats/src', false );
-$tsClassLoader->filterFinalPath( 'DaveChild' . 'Libraries/text-stats/src', '' );
-$tsClassLoader->register();
-$classLoader = new ClassLoader( 'PressForward', dirname( __FILE__ ), false );
-$classLoader->filterFinalPath( 'PressForward' . DIRECTORY_SEPARATOR, '' );
-$classLoader->register();
 
-// use PressForward\Loader;
+// @todo This call to filterFinalPath is likely a bug.
+$ts_class_loader = new ClassLoader( 'DaveChild', 'Libraries/text-stats/src', false );
+$ts_class_loader->filterFinalPath( 'DaveChild/Libraries/text-stats/src', '' );
+$ts_class_loader->register();
+
+$class_loader = new ClassLoader( 'PressForward', __DIR__, false );
+$class_loader->filterFinalPath( 'PressForward' . DIRECTORY_SEPARATOR, '' );
+$class_loader->register();
+
+/**
+ * PressForward instance.
+ *
+ * @param string $prop Optional. Use to fetch a property from the global instance.
+ * @return mixed
+ */
 function pressforward( $prop = false ) {
 	$instance = new stdClass();
 	try {
 		$instance = new PressForward\Application( new Config( ConfigType::PLUGIN, __FILE__ ) );
 		$instance->boot();
-	} catch (Intraxia\Jaxion\Core\ApplicationAlreadyBootedException $e) {
+	} catch ( Intraxia\Jaxion\Core\ApplicationAlreadyBootedException $e ) {
 		$instance = PressForward\Application::instance();
-
 	}
+
 	if ( ! $prop ) {
 		return $instance;
 	} else {
