8bda4256f06209ebfabd20dd0ffcf025e0a4c695
Boone B Gorges <boonebgorges@gmail.com>
2023-02-01 10:33:53 -0600

    Some PHPCS for includes/opml/object.php.
    See #1116.

--- includes/opml/object.php
+++ includes/opml/object.php
@@ -1,38 +1,80 @@
 <?php
+/**
+ * OPML object.
+ *
+ * @package PressForward
+ */
 
+/**
+ * OPML_Object class.
+ */
 class OPML_Object {
-
-	function __construct( $url ) {
+	/**
+	 * Constructor.
+	 *
+	 * @param string $url URL.
+	 */
+	public function __construct( $url ) {
 		$this->url = $url;
 		$this->folders = array();
 		$this->feeds = array();
 	}
 
-	function set_folder( $folder_obj ) {
+	/**
+	 * Sets a folder.
+	 *
+	 * @param object $folder_obj Folder data.
+	 */
+	public function set_folder( $folder_obj ) {
 		$folder_obj->slug = $this->slugify( $folder_obj->title );
 		$this->folders[ $folder_obj->slug ] = $folder_obj;
 	}
-	function set_title( $string ) {
-		if ( empty( $string ) ) {
+
+	/**
+	 * Sets OPML title.
+	 *
+	 * @param string $title Title.
+	 */
+	public function set_title( $title ) {
+		if ( empty( $title ) ) {
 			$this->title = $this->url;
 		} else {
-			$this->title = (string) $string;
+			$this->title = (string) $title;
 		}
 	}
-	function get_title() {
+
+	/**
+	 * Gets OPML title.
+	 *
+	 * @return string
+	 */
+	public function get_title() {
 		if ( empty( $this->title ) ) {
 			return $this->url;
 		} else {
 			return $this->title;
 		}
 	}
-	function get_folder( $key ) {
+
+	/**
+	 * Gets a folder.
+	 *
+	 * @param string $key Folder key.
+	 * @return object
+	 */
+	public function get_folder( $key ) {
 		$folders = $this->folders;
 		$key = $this->slugify( $key );
 		return $folders[ $key ];
-
 	}
-	function set_feed( $feed_obj, $folder = false ) {
+
+	/**
+	 * Sets a feed.
+	 *
+	 * @param object       $feed_obj Feed object.
+	 * @param string|array $folder   Optional. Associated folder(s).
+	 */
+	public function set_feed( $feed_obj, $folder = false ) {
 		if ( ! $folder ) {
 			// Do not set an unsorted feed if it has already been set
 			// as a sorted feed.
@@ -43,13 +85,11 @@ class OPML_Object {
 		} else {
 			if ( isset( $this->feeds[ $feed_obj->id ] ) ) {
 				$feed_obj = $this->feeds[ $feed_obj->id ];
-				// $feed_obj->folder[] = $folder;
-			} elseif ( !empty($feed_obj) && ( empty( $feed_obj->folder ) || ! is_array( $feed_obj->folder ) ) ) {
+
+			} elseif ( !empty( $feed_obj ) && ( empty( $feed_obj->folder ) || ! is_array( $feed_obj->folder ) ) ) {
 				$feed_obj->folder = array();
-				// $feed_obj->folder[] = $folder;
-			} else {
-				// $feed_obj->folder[] = $folder;
 			}
+
 			if ( is_array( $folder ) ) {
 				foreach ( $folder as $folder_type ) {
 					$feed_obj->folder[] = $folder_type;
@@ -57,24 +97,41 @@ class OPML_Object {
 			} else {
 				$feed_obj->folder[] = $folder;
 			}
+
 			$this->feeds[ $feed_obj->id ] = $feed_obj;
 		}
 	}
-	public function check_keys( $array, $keys, $strict = false ) {
-		$array['missing'] = array();
+
+	/**
+	 * Checks for missing keys in an array.
+	 *
+	 * @param array $check  Array to check.
+	 * @param array $keys   Keys to check for.
+	 * @param bool  $strict If true, returns false on failed check.
+	 * @return bool|array
+	 */
+	public function check_keys( $check, $keys, $strict = false ) {
+		$check['missing'] = array();
 		foreach ( $keys as $key ) {
-			if ( ! array_key_exists( $key, $array ) ) {
+			if ( ! array_key_exists( $key, $check, true ) ) {
 				if ( $strict ) {
 					return false;
 				} else {
-					$array[ $key ] = '';
-					$array['missing'][] = $key;
+					$check[ $key ] = '';
+					$check['missing'][] = $key;
 				}
 			}
 		}
 		return $array;
 	}
-	function assure_title_and_text( $entry ) {
+
+	/**
+	 * Ensures that an entry has text and a title.
+	 *
+	 * @param array $entry Entry data.
+	 * @return array
+	 */
+	public function assure_title_and_text( $entry ) {
 		if ( ! empty( $entry['title'] ) && empty( $entry['text'] ) ) {
 			$entry['text'] = $entry['title'];
 		} elseif ( ! empty( $entry['text'] ) && empty( $entry['title'] ) ) {
@@ -83,43 +140,73 @@ class OPML_Object {
 			$entry['text'] = $entry['feedUrl'];
 			$entry['title'] = $entry['feedUrl'];
 		}
+
 		return $entry;
 	}
 
-	function make_a_folder_obj( $entry ) {
+	/**
+	 * Generates a folder object based on an entry.
+	 *
+	 * @param array $entry Entry data.
+	 * @return object
+	 */
+	public function make_a_folder_obj( $entry ) {
 		$folder = new \stdClass();
+
 		$entry = (array) $entry;
 		$entry = $this->check_keys( $entry, array( 'title', 'text' ) );
-		$entry['title'] = ( ! empty( $entry['title'] ) ? $entry['title'] : false);
-		$entry['text'] = ( ! empty( $entry['text'] ) ? $entry['text'] : false);
+
+		$entry['title'] = ! empty( $entry['title'] ) ? $entry['title'] : false;
+		$entry['text']  = ! empty( $entry['text'] ) ? $entry['text'] : false;
+
 		$entry = $this->assure_title_and_text( $entry );
+
 		$folder->title = $entry['title'];
-		$folder->text = $entry['text'];
-		// pf_log('Making folder with title of '.$folder->title);
+		$folder->text  = $entry['text'];
+
 		return $folder;
 	}
-	function make_a_feed_obj( $entry ) {
+
+	/**
+	 * Generates a feed object based on an entry.
+	 *
+	 * @param array $entry Entry data.
+	 * @return object
+	 */
+	public function make_a_feed_obj( $entry ) {
 		$feed = new \stdClass();
+
 		$entry = (array) $entry;
 		if ( empty( $entry['xmlUrl'] ) ) {
 			$entry['xmlUrl'] = $entry['htmlUrl'];
 		}
+
 		if ( empty( $entry['feedUrl'] ) ) {
 			$entry['feedUrl'] = $entry['xmlUrl'];
 		}
+
 		$entry = $this->assure_title_and_text( $entry );
 		$entry = $this->check_keys( $entry, array( 'title', 'text', 'type', 'xmlUrl', 'htmlUrl', 'feedUrl' ) );
-		$feed->title = $entry['title'];
-		$feed->text = $entry['text'];
-		$feed->type = $entry['type'];
-		$feed->xmlUrl = str_replace( '&amp;', '&', $entry['xmlUrl'] );
+
+		$feed->title   = $entry['title'];
+		$feed->text    = $entry['text'];
+		$feed->type    = $entry['type'];
+		$feed->xmlUrl  = str_replace( '&amp;', '&', $entry['xmlUrl'] );
 		$feed->feedUrl = str_replace( '&amp;', '&', $entry['feedUrl'] );
 		$feed->htmlUrl = str_replace( '&amp;', '&', $entry['htmlUrl'] );
-		$feed->id = md5( $feed->feedUrl );
-		// pf_log('Making feed with URL of '.$feed->feedUrl);
+		$feed->id      = md5( $feed->feedUrl );
+
 		return $feed;
 	}
-	function order_opml_entries( $a, $b ) {
+
+	/**
+	 * Sort callback that orders items by folder.
+	 *
+	 * @param object $a Item a.
+	 * @param object $b Item b.
+	 * @return int
+	 */
+	public function order_opml_entries( $a, $b ) {
 		if ( empty( $a->folder ) ) {
 			return 1;
 		}
@@ -140,16 +227,29 @@ class OPML_Object {
 			return 1;
 		}
 	}
-	function order_feeds_by_folder() {
+
+	/**
+	 * Forces feeds to be ordered by folder.
+	 */
+	public function order_feeds_by_folder() {
 		usort( $this->feeds, array( $this, 'order_opml_entries' ) );
 	}
-	function get_feeds_by_folder( $folder ) {
+
+	/**
+	 * Gets feeds belonging to a folder.
+	 *
+	 * @param array $folder Folder data.
+	 * @return array
+	 */
+	public function get_feeds_by_folder( $folder ) {
 		$folder_a = array();
+
 		if ( is_array( $folder ) && ! empty( $folder[0] ) ) {
 			$folder = $folder[0];
 		} elseif ( is_array( $folder ) && ! empty( $folder['slug'] ) ) {
 			$folder = $folder['slug'];
 		}
+
 		foreach ( $this->feeds as $feed ) {
 			if ( ! empty( $feed->folder ) ) {
 				foreach ( $feed->folder as $feed_folder ) {
@@ -159,11 +259,19 @@ class OPML_Object {
 				}
 			}
 		}
+
 		if ( empty( $folder_a ) ) {
 			return false;
 		}
+
 		return $folder_a;
 	}
+
+	/**
+	 * Gets feeds that aren't in a folder.
+	 *
+	 * @return array
+	 */
 	public function get_feeds_without_folder() {
 		$folder_a = array();
 		foreach ( $this->feeds as $feed ) {
@@ -171,14 +279,24 @@ class OPML_Object {
 				$folder_a[] = $feed;
 			}
 		}
+
 		if ( empty( $folder_a ) ) {
 			return false;
 		}
+
 		return $folder_a;
 	}
-	function get_feed_by_id( $unique_id ) {
+
+	/**
+	 * Gets a feed by its uid.
+	 *
+	 * @param int $unique_id Unique ID of the feed.
+	 * @return object
+	 */
+	public function get_feed_by_id( $unique_id ) {
 		return $this->feeds[ $unique_id ];
 	}
+
 	function sanitize( $string, $force_lowercase = true, $anal = false ) {
 		$strip = array(
 		'~',
