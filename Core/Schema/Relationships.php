49658282ce85c275ca238731093496cf82d54a56
Boone B Gorges <boonebgorges@gmail.com>
2023-01-31 19:52:48 -0600

    PHPCS for Core/Schema/Relationships.php.
    See #1116.

--- Core/Schema/Relationships.php
+++ Core/Schema/Relationships.php
@@ -1,23 +1,38 @@
 <?php
+/**
+ * Relationships data control.
+ *
+ * @package PressForward
+ */
+
 namespace PressForward\Core\Schema;
 
 use Intraxia\Jaxion\Contract\Core\HasActions;
-/**
- * Classes and functions for dealing with feed items
- */
 
 /**
- * Database class for manipulating feed items
+ * Database class for manipulating relationships.
  */
 class Relationships implements HasActions {
+	/**
+	 * Relationships database table name.
+	 *
+	 * @access public
+	 * @var string
+	 */
 	public $table_name;
 
+	/**
+	 * Constructor.
+	 */
 	public function __construct() {
 		global $wpdb;
 
 		$this->table_name = $wpdb->prefix . 'pf_relationships';
 	}
 
+	/**
+	 * {@inheritdoc}
+	 */
 	public function action_hooks() {
 		$hooks = array(
 			array(
@@ -29,12 +44,11 @@ class Relationships implements HasActions {
 		return $hooks;
 	}
 
-
 	/**
-	 * Checks to see whether the relationship table needs to be installed, and installs if so
+	 * Checks to see whether the relationship table needs to be installed, and installs if so.
 	 *
 	 * A regular activation hook won't work correctly given where how
-	 * this file is loaded. Might change this in the future
+	 * this file is loaded. Might change this in the future.
 	 */
 	public function maybe_install_relationship_table() {
 		if ( ! current_user_can( 'activate_plugins' ) ) {
@@ -67,7 +81,7 @@ class Relationships implements HasActions {
 	}
 
 	/**
-	 * Defines the relationship table schema and runs dbDelta() on it
+	 * Defines the relationship table schema and runs dbDelta() on it.
 	 */
 	public static function install_relationship_table() {
 		global $wpdb;
@@ -76,25 +90,38 @@ class Relationships implements HasActions {
 
 		$sql   = array();
 		$sql[] = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}pf_relationships (
-						id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+					id bigint(20) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 					user_id bigint(20) NOT NULL,
 					item_id bigint(20) NOT NULL,
-				relationship_type smallint(5) NOT NULL,
-				value varchar(255),
+					relationship_type smallint(5) NOT NULL,
+					value varchar(255),
 
-				KEY user_id (user_id),
-				KEY item_id (item_id),
-				KEY relationship_type (relationship_type)
-			)";
+					KEY user_id (user_id),
+					KEY item_id (item_id),
+					KEY relationship_type (relationship_type)
+		)";
 
 		dbDelta( $sql );
 	}
 
+	/**
+	 * Creates a new relationship record.
+	 *
+	 * @param array $args {
+	 *   @var int    $user_id           User ID.
+	 *   @var int    $item_id           Item ID.
+	 *   @var int    $relationship_type Relationship type.
+	 *   @var string $value             Value.
+	 *   @var bool   $unique            Whether to enforce uniqueness. Default true.
+	 * }
+	 * @return int
+	 */
 	public function create( $args = array() ) {
 		global $wpdb;
 
 		$r = wp_parse_args(
-			$args, array(
+			$args,
+			array(
 				'user_id'           => 0,
 				'item_id'           => 0,
 				'relationship_type' => 0,
@@ -128,19 +155,31 @@ class Relationships implements HasActions {
 
 		$this->clean_relationship_cache_incrementor();
 
-		return $wpdb->insert_id;
+		return (int) $wpdb->insert_id;
 	}
 
 	/**
+	 * Updates a relationship record.
+	 *
 	 * We assume that only the value ever needs to change.
 	 *
-	 * Any other params are interpreted as WHERE conditions
+	 * Any other params are interpreted as WHERE conditions.
+	 *
+	 * @param array $args {
+	 *   @var int    $id                Relationship ID.
+	 *   @var int    $user_id           User ID.
+	 *   @var int    $item_id           Item ID.
+	 *   @var int    $relationship_type Relationship type.
+	 *   @var string $value             Value.
+	 * }
+	 * @return bool
 	 */
 	public function update( $args = array() ) {
 		global $wpdb;
 
 		$r = wp_parse_args(
-			$args, array(
+			$args,
+			array(
 				'id'                => 0,
 				'user_id'           => false,
 				'item_id'           => false,
@@ -149,7 +188,7 @@ class Relationships implements HasActions {
 			)
 		);
 
-		// If an 'id' is passed, use it. Otherwise build a WHERE
+		// If an 'id' is passed, use it. Otherwise build a WHERE.
 		$where        = array();
 		$where_format = array();
 		if ( $r['id'] ) {
@@ -170,7 +209,7 @@ class Relationships implements HasActions {
 
 		$updated = false;
 
-		// Sanity: Don't allow for empty $where
+		// Sanity: Don't allow for empty $where.
 		if ( ! empty( $where ) ) {
 			$updated = $wpdb->update(
 				$this->table_name,
@@ -186,6 +225,18 @@ class Relationships implements HasActions {
 		return (bool) $updated;
 	}
 
+	/**
+	 * Queries for relationship records.
+	 *
+	 * @param array $args {
+	 *   Array of optional arguments.
+	 *   @var int $id                Relationship ID.
+	 *   @var int $user_id           User ID.
+	 *   @var int $item_id           Item ID.
+	 *   @var int $relationship_type Relationship type.
+	 * }
+	 * @return array
+	 */
 	public function get( $args = array() ) {
 		global $wpdb;
 
@@ -251,7 +302,12 @@ class Relationships implements HasActions {
 		return $results;
 	}
 
-	function delete( $args = array() ) {
+	/**
+	 * Deletes records based on params passed.
+	 *
+	 * @param array $args See $args array in \PressForward\Core\Schema\Relationships::get().
+	 */
+	public function delete( $args = array() ) {
 		global $wpdb;
 
 		if ( ! empty( $args['id'] ) ) {
