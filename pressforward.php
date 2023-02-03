3c83c05be3993215b60db73564f9780856780cbd
Boone B Gorges <boonebgorges@gmail.com>
2023-02-03 10:06:39 -0600

    PHPCS for pressforward.php.
    See #1116.

--- pressforward.php
+++ pressforward.php
@@ -1,25 +1,26 @@
 <?php
-/*
-Plugin Name: PressForward
-Plugin URI: http://pressforward.org/
-Description: The PressForward Plugin is a tool by the Roy Rosenzweig Center for History and New Media for aggregating and curating web-based content within the WordPress dashboard.
-Version: 5.3.1
-GitHub Plugin URI: https://github.com/PressForward/pressforward
-Author: Aram Zucker-Scharff, Boone B Gorges, Jeremy Boggs
-Author URI: http://pressforward.org/about/team/
-License: GPL2
-Text Domain: pf
-Domain Path: /languages
-*/
+/**
+ * Plugin Name: PressForward
+ * Plugin URI: http://pressforward.org/
+ * Description: The PressForward Plugin is a tool by the Roy Rosenzweig Center for History and New Media for aggregating and curating web-based content within the WordPress dashboard.
+ * Version: 5.3.1
+ * GitHub Plugin URI: https://github.com/PressForward/pressforward
+ * Author: Aram Zucker-Scharff, Boone B Gorges, Jeremy Boggs
+ * Author URI: http://pressforward.org/about/team/
+ * License: GPL2
+ * Text Domain: pf
+ * Domain Path: /languages
+ *
+ * @package PressForward
+ */
 
-
-// Set up some constants
+// Set up some constants.
 define( 'PF_SLUG', 'pf' );
 define( 'PF_TITLE', 'PressForward' );
 define( 'PF_MENU_SLUG', PF_SLUG . '-menu' );
 define( 'PF_NOM_EDITOR', 'edit.php?post_type=nomination' );
 define( 'PF_NOM_POSTER', 'post-new.php?post_type=nomination' );
-define( 'PF_ROOT', dirname( __FILE__ ) );
+define( 'PF_ROOT', __DIR__ );
 define( 'PF_FILE_PATH', PF_ROOT . '/' . basename( __FILE__ ) );
 define( 'PF_URL', plugins_url( '/', __FILE__ ) );
 define( 'PF_VERSION', '5.3.1' );
@@ -37,20 +38,20 @@ if ( ! class_exists( 'MyCLabs\Enum\Enum' ) ) {
 	require 'Libraries/Enum.php';
 }
 
-$PHPCheck = new WPUpdatePHP( '5.3.0' );
-$PHPCheck->set_plugin_name( 'PressForward' );
+$php_check = new WPUpdatePHP( '5.3.0' );
+$php_check->set_plugin_name( 'PressForward' );
 
-
-if ( (PHP_VERSION < 5.3) || ( ! $PHPCheck->does_it_meet_required_php_version( PHP_VERSION ) ) ) {
+if ( ( PHP_VERSION < 5.3 ) || ( ! $php_check->does_it_meet_required_php_version( PHP_VERSION ) ) ) {
 	wp_die( 'As of version 3.9.0 PressForward requires at least PHP 5.3.' );
 	return;
 } else {
-	require( 'init.php' );
+	require 'init.php';
 }
 
+/**
+ * Loads plugin textdomain.
+ */
 function pressforward_load_plugin_textdomain() {
-	load_plugin_textdomain( 'pf', false, basename( dirname( __FILE__ ) ) . '/languages/' );
+	load_plugin_textdomain( 'pf', false, basename( __DIR__ ) . '/languages/' );
 }
 add_action( 'plugins_loaded', 'pressforward_load_plugin_textdomain' );
-
-// call_user_func(array(new Application(__FILE__), 'boot'));
