16a9d8590cba753673e494a5150bb83ca7ab57a1
Boone B Gorges <boonebgorges@gmail.com>
2023-02-16 13:19:52 -0600

    Move constant definition to its own file.
    This makes it easier to run PHPStan analysis using bootstrapFiles.
    
    See #1148.

--- pressforward.php
+++ pressforward.php
@@ -14,16 +14,7 @@
  * @package PressForward
  */
 
-// Set up some constants.
-define( 'PF_SLUG', 'pf' );
-define( 'PF_TITLE', 'PressForward' );
-define( 'PF_MENU_SLUG', PF_SLUG . '-menu' );
-define( 'PF_NOM_EDITOR', 'edit.php?post_type=nomination' );
-define( 'PF_NOM_POSTER', 'post-new.php?post_type=nomination' );
-define( 'PF_ROOT', __DIR__ );
-define( 'PF_FILE_PATH', PF_ROOT . '/' . basename( __FILE__ ) );
-define( 'PF_URL', plugins_url( '/', __FILE__ ) );
-define( 'PF_VERSION', '5.3.1' );
+require 'constants.php';
 
 // Protect File.
 if ( ! defined( 'WPINC' ) ) {
