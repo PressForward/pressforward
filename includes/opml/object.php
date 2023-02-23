1bc80835edd078a87f8b6bdb1db2dee8b23523ad
Boone B Gorges <boonebgorges@gmail.com>
2023-02-23 15:20:28 -0600

    Documentation fix.

--- includes/opml/object.php
+++ includes/opml/object.php
@@ -104,7 +104,7 @@ class OPML_Object {
 	/**
 	 * Sets a feed.
 	 *
-	 * @param object|bool       $feed_obj Feed object.
+	 * @param object            $feed_obj Feed object.
 	 * @param string|array|null $folder   Optional. Associated folder(s).
 	 */
 	public function set_feed( $feed_obj, $folder = null ) {
