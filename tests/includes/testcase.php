738cd9b57daae7203f4bfacfa9ead2d4f250a193
Boone B Gorges <boonebgorges@gmail.com>
2023-04-27 13:36:43 -0500

    Ensure PF content is deleted after each PHPUnit test.
    For some reason this is not happening as part of the transaction rollback
    in recent PHPUnit.

--- tests/includes/testcase.php
+++ tests/includes/testcase.php
@@ -8,4 +8,26 @@ class PF_UnitTestCase extends TestCase {
 	public function set_up() {
 		$this->factory = new PF_UnitTest_Factory;
 	}
+
+	public function tear_down() {
+		global $wpdb;
+
+		$user_ids = $wpdb->get_col( "SELECT ID FROM {$wpdb->users} WHERE user_login != 'admin'" );
+		foreach ( $user_ids as $user_id ) {
+			wp_delete_user( $user_id );
+		}
+
+		$pf_types = array_map(
+			function( $type ) use ( $wpdb ) {
+				return $wpdb->prepare( '%s', $type );
+			},
+			[ 'nomination', 'pf_feed', 'pf_feed_item' ]
+		);
+		$pf_types_str = implode( ',', $pf_types );
+
+		$post_ids = $wpdb->get_col( "SELECT ID FROM {$wpdb->posts} WHERE post_type IN ({$pf_types_str})" );
+		foreach ( $post_ids as $post_id ) {
+			wp_delete_post( $post_id, true );
+		}
+	}
 }
