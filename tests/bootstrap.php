2ef334afbda8d2fc017bc2b5f4a160041e59ee0d
Boone B Gorges <boonebgorges@gmail.com>
2023-04-27 17:20:40 -0500

    Debug

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,11 @@
 <?php
 
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
+var_Dump( $_tests_dir );
 if ( ! $_tests_dir ) {
 	$_tests_dir = '/tmp/wordpress-tests-lib';
 }
+var_Dump( $_tests_dir );
 
 if ( ! defined( 'PF_DIR_TESTDATA' ) ) {
 	define( 'PF_DIR_TESTDATA', __DIR__ . '/data' );
