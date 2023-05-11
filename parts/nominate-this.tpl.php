681dee407ba6d99a16ed72048c1ca4dc466a8e84
Boone B Gorges <boonebgorges@gmail.com>
2023-05-11 09:26:40 -0500

    Clipboard button for bookmarklet Tools panel.
    See #935.

--- parts/nominate-this.tpl.php
+++ parts/nominate-this.tpl.php
@@ -9,17 +9,52 @@ if ( 'as_paragraph' === $context ) {
 	?>
 		<div class="tool-box">
 			<h3 class="title"><?php esc_html_e( 'Nominate This', 'pressforward' ); ?></h3>
-			<p><?php esc_html_e( 'Nominate This is a bookmarklet: a little app that runs in your browser and lets you grab bits of the web.', 'pressforward' ); ?></p>
+			<p><?php esc_html_e( 'Nominate This is a bookmarklet: a little app that runs in your browser and lets you grab bits of the web. Use Nominate This to clip text, images and videos from any web page. Then edit and add more straight from Nominate This before you save or publish it in a post on your site.', 'pressforward' ); ?></p>
 
-			<p><?php esc_html_e( 'Use Nominate This to clip text, images and videos from any web page. Then edit and add more straight from Nominate This before you save or publish it in a post on your site.', 'pressforward' ); ?></p>
-			<p class="description"><?php esc_html_e( 'Drag-and-drop the following link to your bookmarks bar or right click it and add it to your favorites for a posting shortcut.', 'pressforward' ); ?></p>
-			<p class="pressthis"><a class="button" onclick="return false;" oncontextmenu="if(window.navigator.userAgent.indexOf('WebKit')!=-1||window.navigator.userAgent.indexOf('MSIE')!=-1)jQuery('.pressthis-code').show().find('textarea').focus().select();return false;" href="<?php echo esc_attr( htmlspecialchars( pf_get_shortcut_link() ) ); ?>"><span><?php esc_html_e( 'Nominate This', 'pressforward' ); ?></span></a></p>
-			<div class="pressthis-code" style="display:none;">
-				<p class="description"><?php esc_html_e( 'If your bookmarks toolbar is hidden: copy the code below, open your Bookmarks manager, create new bookmark, type Press This into the name field and paste the code into the URL field.', 'pressforward' ); ?></p>
-				<p><textarea rows="5" cols="120" readonly="readonly"><?php echo esc_html( htmlspecialchars( pf_get_shortcut_link() ) ); ?></textarea></p>
-			</div><br />
+			<p><?php esc_html_e( 'Drag-and-drop the "Nominate This" button to your bookmarks bar, or right-click and add it to your favorites.', 'pressforward' ); ?></p>
+			<p><?php esc_html_e( 'On a mobile device? Use the clipboard icon to copy the bookmarklet code and copy into the URL field of a manually-created bookmark.', 'pressforward' ); ?></p>
+			<p class="pressthis-bookmarklet-wrapper">
+				<a class="pressthis-bookmarklet" onclick="return false;" href="<?php echo htmlspecialchars( pf_get_shortcut_link() ); ?>"><span><?php esc_html_e( 'Nominate This', 'press-this' ); ?></span></a>
+				<button type="button" class="button pressthis-js-toggle js-show-pressthis-code-wrap" aria-expanded="false" aria-controls="pressthis-code-wrap">
+					<span class="dashicons dashicons-clipboard"></span>
+					<span class="screen-reader-text"><?php _e( 'Copy &#8220;Nominate This&#8221; bookmarklet code', 'pressforward' ) ?></span>
+				</button>
+			</p>
+
+			<div class="hidden js-pressthis-code-wrap clear" id="pressthis-code-wrap">
+				<p id="pressthis-code-desc">
+					<?php _e( 'If you can&#8217;t drag the bookmarklet to your bookmarks, copy the following code and create a new bookmark. Paste the code into the new bookmark&#8217;s URL field.', 'press-this' ) ?>
+				</p>
+
+				<p>
+					<textarea class="js-pressthis-code" rows="5" cols="120" readonly="readonly" aria-labelledby="pressthis-code-desc"><?php echo htmlspecialchars( pf_get_shortcut_link() ); ?></textarea>
+				</p>
+			</div>
+
+			<script>
+			jQuery( document ).ready( function( $ ) {
+				var $showPressThisWrap = $( '.js-show-pressthis-code-wrap' );
+				var $pressthisCode = $( '.js-pressthis-code' );
+
+				$showPressThisWrap.on( 'click', function( event ) {
+					var $this = $( this );
+
+					$this.parent().next( '.js-pressthis-code-wrap' ).slideToggle( 200 );
+					$this.attr( 'aria-expanded', $this.attr( 'aria-expanded' ) === 'false' ? 'true' : 'false' );
+				});
+
+				// Select Press This code when focusing (tabbing) or clicking the textarea.
+				$pressthisCode.on( 'click focus', function() {
+					var self = this;
+					setTimeout( function() { self.select(); }, 50 );
+				});
+			});
+			</script>
+
+			<br /><br />
 
 			<div>
+
 			<h3><?php esc_html_e( 'Nominate This Extension', 'pressforward' ); ?></h3>
 			<p><?php esc_html_e( 'Nominate This now has an extension for Chrome with additional functionality.', 'pressforward' ); ?></p>
 			<p><a href="https://github.com/PressForward/PressForwardChromeExtension/releases" target="_blank"><?php esc_html_e( 'You can download this new extension at our GitHub repository.', 'pressforward' ); ?></a></p>
@@ -44,7 +79,6 @@ if ( 'as_paragraph' === $context ) {
 					<p><?php esc_html_e( 'Nominate This is a bookmarklet: a little app that runs in your browser and lets you grab bits of the web.', 'pressforward' ); ?></p>
 
 					<p><?php esc_html_e( 'Use Nominate This to clip text, images and videos from any web page. Then edit and add more straight from Nominate This before you save or publish it in a post on your site.', 'pressforward' ); ?></p>
-					<p class="description"><?php esc_html_e( 'Drag-and-drop the following link to your bookmarks bar or right click it and add it to your favorites for a posting shortcut.', 'pressforward' ); ?></p>
 					<p class="pressthis"><a class="button" onclick="return false;" oncontextmenu="if(window.navigator.userAgent.indexOf('WebKit')!=-1||window.navigator.userAgent.indexOf('MSIE')!=-1)jQuery('.pressthis-code').show().find('textarea').focus().select();return false;" href="<?php echo esc_attr( htmlspecialchars( pf_get_shortcut_link() ) ); ?>"><span><?php esc_html_e( 'Nominate This', 'pressforward' ); ?></span></a></p>
 					<div class="pressthis-code" style="display:none;">
 						<p class="description"><?php esc_html_e( 'If your bookmarks toolbar is hidden: copy the code below, open your Bookmarks manager, create new bookmark, type Press This into the name field and paste the code into the URL field.', 'pressforward' ); ?></p>
