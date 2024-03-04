6f822bce973e90c1e21f3e45cb48dd59e5491b13
Boone B Gorges <boonebgorges@gmail.com>
2024-03-04 10:38:44 -0600

    Use better markup and JS event listeners for Stats page events.
    * Using <button> rather than <a> is more semantic, and also avoids problems with unwanted page refresh.
    * Moving onclick events to separate JS listeners instead of inline HTML attributes is more maintainable and easier to read.
    
    See #933.

--- parts/pf-tools/tab-stats.tpl.php
+++ parts/pf-tools/tab-stats.tpl.php
@@ -12,8 +12,8 @@ wp_enqueue_script( 'pf-api' );
 	<div style="width: 100%">
 		<p><?php esc_html_e( 'About your PressForward Install (Requires the WordPress REST API.)', 'pressforward' ); ?></p>
 		<p id="top-level"></p>
-		<a href="#" id="build-authors" class="button" onclick="window.pf.stats.authors.getLeaderboard(true);"><?php esc_html_e( 'Assemble Author Stats', 'pressforward' ); ?></a>
-		<a href="#" id="build-valid-posts" class="button" onclick="window.pf.stats.valid_posts.getLeaderboard(true);"><?php esc_html_e( 'Assemble Valid Posts for Stats', 'pressforward' ); ?></a>
+		<button id="build-authors" class="button button-secondary"><?php esc_html_e( 'Assemble Author Stats', 'pressforward' ); ?></button>
+		<button id="build-valid-posts" class="button button-secondary"><?php esc_html_e( 'Assemble Valid Posts for Stats', 'pressforward' ); ?></button>
 	</div>
 	<div id="stats-app"></div>
 	<div id="author-leaderboard" style="width: 49%; float: left;">
