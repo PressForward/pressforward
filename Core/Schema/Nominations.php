PressForward/pressforward

commit dee173f0f38bbbe944a7001fa2a427cf04b11365

Improved notification PT registration, for compatibility with lower caps.
See #1223.

Core/Schema/Nominations.php
@@ -59,6 +59,11 @@ class Nominations implements HasActions, HasFilters {
 				'hook'   => 'manage_edit-nomination_sortable_columns',
 				'method' => 'nomination_sortable_columns',
 			),
+			array(
+				'hook'   => 'user_has_cap',
+				'method' => 'add_assign_post_tags_cap',
+				'args'   => 4,
+			),
 		);
 	}
 
@@ -87,11 +92,10 @@ class Nominations implements HasActions, HasFilters {
 			// I want a UI for users to use, so true.
 			'show_ui'              => true,
 			// But not the default UI, we want to attach it to the plugin menu.
-			'show_in_menu'         => false,
+			'show_in_menu'         => 'pf-menu',
 			'show_in_rest'         => true,
 			// Linking in the metabox building function.
 			'register_meta_box_cb' => array( $this, 'nominations_meta_boxes' ),
-			'capability_type'      => 'post',
 			// The type of input (besides the metaboxes) that it supports.
 			'supports'             => array( 'title', 'editor', 'thumbnail', 'custom-fields', 'revisions' ),
 			// I think this is set to false by the public argument, but better safe.
@@ -99,7 +103,63 @@ class Nominations implements HasActions, HasFilters {
 			'taxonomies'           => array( 'category', 'post_tag' ),
 		);
 
+		$cap_keys = [
+			'edit_post',
+			'read_post',
+			'delete_post',
+			'edit_posts',
+			'edit_others_posts',
+			'publish_posts',
+			'read_private_posts',
+			'delete_posts',
+			'delete_private_posts',
+			'delete_published_posts',
+			'delete_others_posts',
+			'edit_private_posts',
+			'edit_published_posts',
+			'create_posts',
+		];
+
+		$caps = array_fill_keys(
+			$cap_keys,
+			get_option(
+				'pf_menu_nominate_this_access',
+				pressforward( 'controller.users' )->pf_get_defining_capability_by_role( 'contributor' )
+			)
+		);
+
+		$args['capabilities'] = $caps;
+		$args['map_meta_cap'] = false;
+
 		register_post_type( $this->post_type, $args );
+	}
+
+	/**
+	 * Grants users with the 'pf_menu_nominate_this_access' capability the ability to assign tags to nominations.
+	 *
+	 * @param array $allcaps All capabilities of the user.
+	 * @return array
+	 */
+	public function add_assign_post_tags_cap( $allcaps, $caps, $args, $user) {
+		$base_cap = get_option(
+			'pf_menu_nominate_this_access',
+			pressforward( 'controller.users' )->pf_get_defining_capability_by_role( 'contributor' )
+		);
+
+		if ( ! defined( 'REST_REQUEST' ) || ! REST_REQUEST ) {
+			return $allcaps;
+		}
+
+		global $wp;
+		if ( empty( $wp->query_vars['rest_route'] ) || 0 !== strpos( $wp->query_vars['rest_route'], '/wp/v2/nomination' ) ) {
+			return $allcaps;
+		}
+
+		if ( in_array( 'edit_posts', $caps, true ) && user_can( $user->ID, $base_cap ) ) {
+			$allcaps['edit_posts'] = true;
+		}
+
+		return $allcaps;
 	}
 
 	/**

Core/Bookmarklet/NominateThisCore.php
@@ -696,11 +696,16 @@ class NominateThisCore implements HasActions, HasFilters {
 	 * @return void
 	 */
 	public function register_nomination_success_panel() {
+		$base_cap = get_option(
+			'pf_menu_nominate_this_access',
+			pressforward( 'controller.users' )->pf_get_defining_capability_by_role( 'contributor' )
+		);
+
 		add_submenu_page(
 			'',
 			__( 'Nomination Success', 'pressforward' ),
 			__( 'Nomination Success', 'pressforward' ),
-			'edit_posts',
+			$base_cap,
 			'pf-nomination-success',
 			[ $this, 'nomination_success_panel' ]
 		);

commit 141b6097f9c4d66d3375b3ed24b21375a1b5ebf8

Prevent recursion when mapping 'edit_posts' capability.
Since #1223, a mapped capability has been used to grant users with the ability
to add tags to nominations. The primitive cap used here is 'edit_posts', which
WP itself uses as a heuristic for whether a user ought to be able to add
tags to content. However, the change introduced a problem when the custom
Nominate This role was set to Contributor, because Contributors are identified
in the PF codebase by the 'edit_posts' capability. As a result, the logic introduce
in dee173f0f38bbbe944a7001fa2a427cf04b11365 caused an infinite loop.

We fix this by unhooking the cap-mapping filter and checking the stored (unfiltered)
caps for the current user.

See #1234.

Core/Schema/Nominations.php
@@ -167,7 +167,12 @@ class Nominations implements HasActions, HasFilters {
 			return $allcaps;
 		}
 
-		if ( in_array( 'edit_posts', $caps, true ) && user_can( $user->ID, $base_cap ) ) {
+		// Prevent recursion when checking to see whether the user is assigned this cap.
+		remove_filter( 'user_has_cap', array( $this, 'add_assign_post_tags_cap' ), 10 );
+		$user_has_base_cap = user_can( $user->ID, $base_cap );
+		add_filter( 'user_has_cap', array( $this, 'add_assign_post_tags_cap' ), 10, 4 );
+
+		if ( in_array( 'edit_posts', $caps, true ) && $user_has_base_cap ) {
 			$allcaps['edit_posts'] = true;
 		}
 